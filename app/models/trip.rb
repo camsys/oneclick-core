cdbb4386c77e0456f08f7430494da055e250f3a0
Nicolas Garcia <ngarcia@camsys.com>
2017-01-30 15:22:06 -0500

    Trip itineraries and places are dependent: :destroy

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :user
-  has_many :itineraries
-  belongs_to :origin, class_name: 'Place', foreign_key: :origin_id
-  belongs_to :destination, class_name: 'Place', foreign_key: :destination_id
+  has_many :itineraries, dependent: :destroy
+  belongs_to :origin, class_name: 'Place', foreign_key: :origin_id, dependent: :destroy
+  belongs_to :destination, class_name: 'Place', foreign_key: :destination_id, dependent: :destroy
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
   it { should belong_to :user }
-  it { should have_many :itineraries }
-  it { should belong_to(:origin).class_name('Place') }
-  it { should belong_to(:destination).class_name('Place') }
+  it { should have_many(:itineraries).dependent(:destroy) }
+  it { should belong_to(:origin).class_name('Place').dependent(:destroy) }
+  it { should belong_to(:destination).class_name('Place').dependent(:destroy) }
 end
