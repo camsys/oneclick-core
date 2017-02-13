4f5b068e18be0d2124c78e1d964f5d473594db5d
Nicolas Garcia <ngarcia@camsys.com>
2017-02-13 16:04:23 -0500

    Handle failed user authentication of API calls via failure app
    - Raise an exception if authentication of user token fails
    - If request is */*, sends back response as JSON
    - There may be a better way to test for API requests, but seems like all the Postman requests at least are */*

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,9 +1,8 @@
 module Api
   class ApiController < ApplicationController
     protect_from_forgery prepend: true
-    acts_as_token_authentication_handler_for User, fallback: :none
+    acts_as_token_authentication_handler_for User, fallback: :exception
     respond_to :json
-
 
   end
 end
