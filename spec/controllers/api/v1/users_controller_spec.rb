ea8949c0fa005764dfe2f5539b1020499290518c
Derek Edwards <dedwards8@gmail.com>
2017-02-27 21:22:12 -0500

    Test for Preferred Locale in User Profile call.

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :controller do
 
   let!(:traveler) { FactoryGirl.create :user }
+  let!(:english_traveler) { FactoryGirl.create :english_speaker }
 
   it 'returns the first and last name of a user profile' do
     sign_in traveler
@@ -18,6 +19,21 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     parsed_response = JSON.parse(response.body)
     expect(parsed_response["first_name"]).to eq("Bob")
     expect(parsed_response["last_name"]).to eq("Bobson")
+  end
+
+  it 'returns the first and last name of a user profile' do
+    sign_in english_traveler
+
+    request.headers['X-User-Token'] = english_traveler.authentication_token
+    request.headers['X-User-Email'] = "george@co.uk"
+    get :profile, format: :json
+
+    # test for the 200 status-code
+    expect(response).to be_success
+
+    # Check on a specific translation
+    parsed_response = JSON.parse(response.body)
+    expect(parsed_response["lang"]).to eq("en")
   end
 
 end
