2e581d5acce40e4f90a669e504a4f981d6cd31e0
Derek Edwards <dedwards8@gmail.com>
2017-02-27 21:06:57 -0500

    Add basic user profile test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,11 @@ module Api
 
         hash = {email: @traveler.email, first_name: @traveler.first_name, last_name: @traveler.last_name}
         hash[:lang] = @traveler.preferred_locale.nil? ? nil : @traveler.preferred_locale.name
+        
+        #locale = @traveler.preferred_locale || Locale.first
+        #hash[:hello] = SimpleTranslationEngine.translate(locale.name, 'hello')
         #hash[:characteristics] = @traveler.characteristics_hash
+        #@traveler.user_characteristics.collect{ |c| {name: TranslationEngine.translate_text(c.characteristic.name), code: c.characteristic.code, note: TranslationEngine.translate_text(c.characteristic.note), value: c.value}}
         #hash[:accommodations] = @traveler.accommodations_hash
         #hash[:preferred_modes] = @traveler.preferred_modes_hash
 
