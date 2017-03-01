ecf9612ea99589a970e54ee796fb883adf480d9b
Nicolas Garcia <ngarcia@camsys.com>
2017-03-01 12:48:23 -0500

    Update Fog ENV Variable Names

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_KEY'] || "none",
-      :aws_secret_access_key  => ENV['S3_SECRET'] || "none"
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'] || "none",
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] || "none"
       # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
   }
-  config.fog_directory  = ENV['S3_BUCKET'] || "none"
+  config.fog_directory  = ENV['AWS_BUCKET'] || "none"
 end
