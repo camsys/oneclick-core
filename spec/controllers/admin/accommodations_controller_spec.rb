2546b69a03b7e34057321fced363ee2e45f40279
Nicolas Garcia <ngarcia@camsys.com>
2017-03-03 15:35:49 -0500

    Fix spec bug with accommodation/eligibility counts

--- spec/controllers/admin/accommodations_controller_spec.rb
+++ spec/controllers/admin/accommodations_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Admin::AccommodationsController, type: :controller do 
+RSpec.describe Admin::AccommodationsController, type: :controller do
 
   let!(:admin) { FactoryGirl.create :admin }
   let!(:non_admin) { FactoryGirl.create :user }
-  
+
   it 'gets a list of all accommodations' do
     sign_in admin
 
@@ -27,34 +27,36 @@ RSpec.describe Admin::AccommodationsController, type: :controller do
     sign_in admin
     params = {accommodation: {code: 'Test accommodAtion'}}
     post :create, params: params, format: :js
+    count = Accommodation.count
 
     # test for the 302 status-code (redirect)
     expect(response).to have_http_status(302)
 
     # Confirm that the variable was set
-    expect(Accommodation.count).to eq(1)
+    expect(Accommodation.count).to eq(count + 1)
 
     # Confirm that the code was set to snake case
     expect(Accommodation.last.code).to eq('test_accommodation')
 
   end
 
-  it 'creates and destroys an accommodation' do 
+  it 'creates and destroys an accommodation' do
     sign_in admin
     params = {accommodation: {code: 'Test DeLEte& accommodation22'}}
     post :create, params: params, format: :js
+    count = Accommodation.count
 
     # Confirm that the variable was set
-    expect(Accommodation.count).to eq(1)
+    expect(Accommodation.count).to eq(count + 1)
 
     # Confirm that the code was set to snake case
     expect(Accommodation.last.code).to eq('test_delete_accommodation22')
 
     params = {id: Accommodation.last.id}
-    delete :destroy, params: params, format: :js 
+    delete :destroy, params: params, format: :js
 
     # Confirm that there are no eligibilities
-    expect(Accommodation.count).to eq(0)
+    expect(Accommodation.count).to eq(count)
   end
 
 end
--- spec/controllers/admin/eligibilities_controller_spec.rb
+++ spec/controllers/admin/eligibilities_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Admin::EligibilitiesController, type: :controller do 
+RSpec.describe Admin::EligibilitiesController, type: :controller do
 
   let!(:admin) { FactoryGirl.create :admin }
   let!(:non_admin) { FactoryGirl.create :user }
-  
+
   it 'gets a list of all eligibilities' do
     sign_in admin
 
@@ -27,34 +27,36 @@ RSpec.describe Admin::EligibilitiesController, type: :controller do
     sign_in admin
     params = {eligibility: {code: 'Test eligibility'}}
     post :create, params: params, format: :js
+    count = Eligibility.count
 
     # test for the 302 status-code (redirect)
     expect(response).to have_http_status(302)
 
     # Confirm that the variable was set
-    expect(Eligibility.count).to eq(1)
+    expect(Eligibility.count).to eq(count + 1)
 
     # Confirm that the code was set to snake case
     expect(Eligibility.last.code).to eq('test_eligibility')
 
   end
 
-  it 'creates and destroys an eligibility' do 
+  it 'creates and destroys an eligibility' do
     sign_in admin
     params = {eligibility: {code: 'Test DeLEte& eligibility22'}}
     post :create, params: params, format: :js
+    count = Eligibility.count
 
     # Confirm that the variable was set
-    expect(Eligibility.count).to eq(1)
+    expect(Eligibility.count).to eq(count + 1)
 
     # Confirm that the code was set to snake case
     expect(Eligibility.last.code).to eq('test_delete_eligibility22')
 
     params = {id: Eligibility.last.id}
-    delete :destroy, params: params, format: :js 
+    delete :destroy, params: params, format: :js
 
     # Confirm that there are no eligibilities
-    expect(Eligibility.count).to eq(0)
+    expect(Eligibility.count).to eq(count)
   end
 
 end
