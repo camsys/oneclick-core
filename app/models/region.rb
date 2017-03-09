6406a20df46c7e1d7ce7689499e706ec3a86724d
Nicolas Garcia <ngarcia@camsys.com>
2017-03-09 16:00:44 -0500

    Finish up GeoKitchen module implementation

--- app/models/region.rb
+++ app/models/region.rb
@@ -3,22 +3,21 @@ class Region < ApplicationRecord
 
   ### Attributes ###
   serialize :recipe, GeoRecipe
-  # validates :recipe, geo_recipe: true
 
   ### Callbacks ###
   before_save :build_geometry_from_recipe
 
   ### Methods ###
 
-  # # Custom getter for recipe column returns a GeoRecipe object
-  # def recipe
-  #   GeoRecipe.new(@recipe)
-  # end
-
   private
 
   def build_geometry_from_recipe
     self.geom = recipe.make
+    unless recipe.errors.empty?
+      recipe.errors.each do |e|
+        @errors.add(:recipe, e)
+      end
+    end
   end
 
 end
--- app/services/geo_kitchen.rb
+++ app/services/geo_kitchen.rb
@@ -1,63 +1,104 @@
 module GeoKitchen
-  # Custom validator determines that recipe is a hash with arrays as key values
-  class GeoRecipeValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      if value.is_a?(GeoRecipe)
-        recipe = value.to_h
-        if recipe.values.all?{|v| v.is_a?(Array)}
-          if recipe.values.flatten.all?{|el| el.is_a?(Hash)}
-          else
-            record.errors.add(attribute, "Geography type array elements must all be hashes")
-          end
-        else
-          record.errors.add(attribute, "Each geography type must have an array as value")
-        end
-      else
-        record.errors.add(attribute, "Must be a GeoRecipe object")
-      end
-    end
-  end
 
-
+  # GeoRecipe is basically a list of GeoIngredients, and can "make" itself into a unified geometry
   class GeoRecipe
-    attr_reader :hash
+    attr_reader :ingredients, :errors
 
-    def initialize(recipe_hash={})
-      @hash = recipe_hash
+    # Takes an array of GeoIngredients
+    def initialize(ingredients=[])
+      @ingredients = ingredients.select {|i| i.is_a?(GeoIngredient)}
+      @errors = []
+      invalid_count = ingredients.length - @ingredients.length
+      @errors << "#{invalid_count} arguments were not GeoIngredients" if invalid_count > 0
     end
 
+    # Combine all the ingredients' geometries into a single unified geom
     def make
-      puts "Making GeoRecipe into a geometry...", hash.ai
-      geom = hash.map do |type, areas|
-        geo_model = type.to_s.classify.constantize
-        geoms = areas.map {|area| geo_model.find_by(area).geom }
-        geoms.reduce { |combined_area, geom| combined_area.union(geom) }
-      end.reduce { |combined_area, geom| combined_area.union(geom) }
-      return geom
+      @ingredients.map do |ingredient|
+        geom = ingredient.to_geom
+        if geom
+          geom
+        else
+          @errors << "#{ingredient.to_s} could not be converted to a geometry."
+          nil
+        end
+      end.compact.reduce {|combined_area, geom| combined_area.union(geom)}
     end
 
-    def to_h
-      @hash
+    # Map its consituent ingredients to hashes
+    def to_a
+      @ingredients.map {|i| i.to_h}
+    end
+
+    # First convert to array, then to a string
+    def to_s
+      to_a.to_s
+    end
+
+    # For pretty printing
+    def ai
+      to_a.ai
     end
 
     private
 
     # GeoRecipe#load and #dump allow this to respond to Rails serialize
-    def self.load(recipe_hash)
-      self.new(eval(recipe_hash || "{}"))
+    def self.load(ingredients_array)
+      ingredients_array = eval(ingredients_array || "[]")
+      self.new(ingredients_array.map {|i| GeoIngredient.load(i)})
     end
 
     def self.dump(obj)
-
       unless obj.is_a?(self)
         raise ::ActiveRecord::SerializationTypeMismatch,
           "Attribute was supposed to be a #{self}, but was a #{obj.class}. -- #{obj.inspect}"
       end
-
-      obj.to_h.to_s # Return a stringified hash
+      obj.to_s # Return a stringified array
     end
   end
 
   class GeoIngredient
+    attr_accessor :model, :attributes
+
+    # Takes the name of a Database table model, and a list of attributes to identify it uniquely
+    def initialize(model_name, attributes={})
+      @model = model_name.to_s.classify.constantize
+      @attributes = attributes
+    end
+
+    # Find the db record this ingredient refers to
+    def find_record
+      @model.find_by(@attributes)
+    end
+
+    # Get the geometry of the record this ingredient refers to
+    def to_geom
+      record = find_record
+      record ? record.geom : false
+    end
+
+    # Converts to hash of key attributes
+    def to_h
+      {
+        model: @model.name,
+        attributes: @attributes
+      }
+    end
+
+    # Converts to hash and then to string
+    def to_s
+      @attributes.values.join(' - ')
+    end
+
+    # For pretty printing
+    def ai
+      to_h.ai
+    end
+
+    # Load method for serializing; called by GeoRecipe
+    def self.load(hash)
+      self.new(hash[:model], hash[:attributes])
+    end
+
   end
 end
