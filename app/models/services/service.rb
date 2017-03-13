fd331a2c79335035cf1f5acd31da31734f42cc66
Nicolas Garcia <ngarcia@camsys.com>
2017-03-13 14:49:34 -0400

    Add Coverage Areas to Service Table, Associations to Model

--- app/models/services/service.rb
+++ app/models/services/service.rb
@@ -7,6 +7,8 @@ class Service < ApplicationRecord
   has_many :itineraries
   has_and_belongs_to_many :accommodations
   has_and_belongs_to_many :eligibilities
+  belongs_to :start_or_end_area, class_name: 'Region', foreign_key: :start_or_end_area_id, dependent: :destroy
+  belongs_to :trip_within_area, class_name: 'Region', foreign_key: :trip_within_area_id, dependent: :destroy
 
   ### Validations ###
   validates_presence_of :name, :type
