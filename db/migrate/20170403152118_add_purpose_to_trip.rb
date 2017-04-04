dce4f664acbc9d26ca7f8267e173dff155b731eb
Derek Edwards <dedwards8@gmail.com>
2017-04-03 21:47:53 -0400

    Revert "Services are available by purpose. (#37)" (#38)
    This reverts commit d439efb88d2ca51980424f55039a69a6c9ab5b63.

--- db/migrate/20170403152118_add_purpose_to_trip.rb
+++
@@ -1,5 +0,0 @@
-class AddPurposeToTrip < ActiveRecord::Migration[5.0]
-  def change
-  	add_reference :trips, :purpose, index: true, foreign_key: true
-  end
-end
--- spec/factories/purposes.rb
+++ spec/factories/purposes.rb
@@ -1,11 +1,5 @@
 FactoryGirl.define do
   factory :purpose do
-
-    code 'medical'
-
-    factory :metallica_concert do
-      code "metallica_concert"
-    end
-
+    
   end
 end
