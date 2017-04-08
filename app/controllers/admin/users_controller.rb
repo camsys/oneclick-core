e5b0c7c9b28bf44129ecbe466804e612357e0980
Derek Edwards <dedwards8@gmail.com>
2017-04-08 15:28:04 -0400

    Add ability to create staff from the staff page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,23 @@
 class Admin::UsersController < Admin::AdminController
 
   def index
-    @staff = User.all.order(:email)
+    @staff = User.staff.order(:last_name, :first_name, :email)
+    @new_user= User.new 
+    @roles = Role.all 
+  end
+
+  def create
+  	roles = params[:user].delete :roles
+  	role = Role.find_by(id: roles)
+  	new_user = User.create(user_params)
+  	role ? new_user.roles << role : nil
+  	redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation, :roles)
   end
 
 end
