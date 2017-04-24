02e40a4415687aab824f6b3e7bb9c35060b65ba7
Nicolas Garcia <ngarcia@camsys.com>
2017-04-24 17:20:49 -0400

    Ran rake db:migrate after merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424154009) do
+ActiveRecord::Schema.define(version: 20170424195259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
