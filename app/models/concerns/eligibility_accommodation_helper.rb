581a98a48d018acbb56964051751bdf5276b10ee
Derek Edwards <dedwards8@gmail.com>
2017-06-19 11:29:36 -0400

    DRY Up translations for purposes, accs, and eligs

--- app/models/concerns/eligibility_accommodation_helper.rb
+++ app/models/concerns/eligibility_accommodation_helper.rb
@@ -4,4 +4,21 @@ module EligibilityAccommodationHelper
   	 self.code = self.code.parameterize.underscore
   end
 
+  # To Label is used by SimpleForm to Get the Label
+  def to_label locale=:en
+    self.name locale
+  end
+
+  def name locale=:en
+    SimpleTranslationEngine.translate(locale, "#{self.class.name.downcase}_#{self.code}_name")
+  end
+
+  def note locale=:en
+    SimpleTranslationEngine.translate(locale, "#{self.class.name.downcase}_#{self.code}_note")
+  end
+
+  def question locale=:en
+    SimpleTranslationEngine.translate(locale, "#{self.class.name.downcase}_#{self.code}_question")
+  end
+
 end
--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -9,13 +9,4 @@ class Purpose < ApplicationRecord
 
   before_save :snake_casify
 
-  # To Label is used by SimpleForm to Get the Label
-  def to_label locale=:en
-  	SimpleTranslationEngine.translate(locale, "purpose_#{self.code}_name")
-  end
-  
-  def name locale=:en
-    SimpleTranslationEngine.translate(locale, "purpose_#{self.code}_name")
-  end
-
 end
