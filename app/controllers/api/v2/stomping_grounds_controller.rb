6f2b5d41e523078149492a004b9771559fba18bb
Derek Edwards <dedwards8@gmail.com>
2017-06-22 15:56:41 -0400

    Delete a stomping_ground

--- app/controllers/api/v2/stomping_grounds_controller.rb
+++ app/controllers/api/v2/stomping_grounds_controller.rb
@@ -8,6 +8,16 @@ module Api
         stomping_grounds_hash = @traveler.stomping_grounds.map {|sg| StompingGroundSerializer.new(sg).to_hash}
         render(success_response(stomping_grounds_hash))
       end
+
+      def destroy
+        stomping_ground = @traveler.stomping_grounds.find_by(id: params[:id]) 
+        if stomping_ground
+          stomping_ground.delete
+          render(success_response(message: "deleted"))
+        else
+          render(fail_response(status: 404, message: "Not Found"))
+        end
+      end
       
     end
   end
