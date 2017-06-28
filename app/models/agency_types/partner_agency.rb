041de452fafc30159b61599173ee758d2a5330fd
Nicolas Garcia <ngarcia@camsys.com>
2017-06-28 11:06:37 -0400

    Allow Partner and Transpo Agencies to have services

--- app/models/agency_types/partner_agency.rb
+++ app/models/agency_types/partner_agency.rb
@@ -1,5 +1,9 @@
+# Partner agency staff have broad permissions to view reports and other information.
+# They may or may not have transportation services
 class PartnerAgency < Agency
   
   resourcify  # user roles may be scoped to agencies
+  
+  has_many :services
     
 end
--- app/models/agency_types/transportation_agency.rb
+++ app/models/agency_types/transportation_agency.rb
@@ -1,3 +1,5 @@
+# Transportation Agencies have transportation services, and their staff may
+# only view reports relevant to the agency
 class TransportationAgency < Agency
 
   resourcify  # user roles may be scoped to transportation agencies
