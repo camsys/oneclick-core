a8c24c62f904d6a5019409526847a27a533ba7f3
Nicolas Garcia <ngarcia@camsys.com>
2017-06-30 15:20:16 -0400

    Attempt to get heroku to compile

--- config/initializers/rgeo.rb
+++ config/initializers/rgeo.rb
@@ -1,4 +1,4 @@
 RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config| 
-  # config.default = RGeo::Geographic.spherical_factory(srid: 4326) 
-  config.default = RGeo::Geos::CAPIFactory.new(:srid => 4326)
+  config.default = RGeo::Geographic.spherical_factory(:srid => 4326) 
+  # config.default = RGeo::Geos::CAPIFactory.new(:srid => 4326)
 end
