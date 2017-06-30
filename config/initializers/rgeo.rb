e7f26b48959cbc8ee9659df7a01b5b0964d276ce
Nicolas Garcia <ngarcia@camsys.com>
2017-06-30 15:52:18 -0400

    Need to use CAPI Factory for geo operations to work

--- config/initializers/rgeo.rb
+++ config/initializers/rgeo.rb
@@ -1,4 +1,3 @@
 RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config| 
-  config.default = RGeo::Geographic.spherical_factory(:srid => 4326) 
-  # config.default = RGeo::Geos::CAPIFactory.new(:srid => 4326)
+  config.default = RGeo::Geos::CAPIFactory.new(:srid => 4326)
 end
