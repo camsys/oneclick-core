646e3a0837f0f4fa7258b7e6d4201c06e2f49279
Nicolas Garcia <ngarcia@camsys.com>
2017-06-30 16:14:58 -0400

    Fix Geos bug: only set default factory if Geos are supported

--- config/initializers/rgeo.rb
+++ config/initializers/rgeo.rb
@@ -1,9 +1,10 @@
 RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
-  puts "CHECKING RGEO SUPPORT..."
+  
+  # Only set default factory if Geos are supported. For some reason this breaks
+  # Heroku deployment if assets aren't precompiled, but it works fine after
+  # deployment is complete.
   if RGeo::Geos.supported?
-    puts "GEOS SUPPORTED!"
     config.default = RGeo::Geos::CAPIFactory.new(:srid => 4326)
-  else
-    puts "GEOS NOT SUPPORTED :("
   end
+  
 end
