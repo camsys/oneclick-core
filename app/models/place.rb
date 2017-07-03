c154c1631e531804f59625d9b06cf75b72677e66
Derek Edwards <dedwards8@gmail.com>
2017-07-02 20:19:06 -0400

    Search Stomping Grounds and Create v2 Places Search Call (#81)
    * Add v2 landmark search
    
    * Add StompingGround Search
    
    * Add stomping ground tests"

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,6 +6,17 @@ class Place < ApplicationRecord
   #### Includes ####
   include GooglePlace
 
+  # Search over all classes that inherit from place by query string
+  def self.get_by_query_str(query_str, limit, user=nil)
+    rel = nil
+    if user
+      rel = self.where(user: user).arel_table[:name].lower().matches(query_str)
+    else
+      rel = self.arel_table[:name].lower().matches(query_str)
+    end
+    self.where(rel).limit(limit)
+  end
+
   # If a google_place_attributes param is passed, will create a Place based on the JSON contained therein.
   def self.new attrs=nil
     if attrs && attrs[:google_place_attributes]
