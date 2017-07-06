64c2e8a3503a54c2015e398ec970c787b81322ca
Nicolas Garcia <ngarcia@camsys.com>
2017-07-06 12:07:13 -0400

    Switch Dashboard back button to a link, refactor back link into a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
 
-  # Allows named yields within partial layouts
-  def yield_content(content_key)
-    view_flow.content.delete(content_key)
-  end
 
+  ### FORM INPUT NAME HELPERS ###
+  # Help build custom forms that communicate properly with controllers
+  
   # Construct a name for an input control appropriate for setting nested attributes
   def input_name form_builder, nest, attribute=nil, count=nil
     name = form_builder.object_name + '[' + nest.to_s + '_attributes]['
@@ -17,9 +16,14 @@ module ApplicationHelper
   def input_name_for(object, *attributes)
     object.class.name.underscore + attributes.map{|att| "[#{att.to_s}]"}.join
   end
+  
+  ### form input name helpers ###
+  
 
-  ###
+
+  ### REMOTE PARTIAL HELPERS ###
   # Helpers for handling naming and rendering of remote form partials
+  
   def partial_path
     self.instance_variable_get(:@virtual_path)
   end
@@ -37,6 +41,26 @@ module ApplicationHelper
     "<input class='hidden' name='partial_path' type'text' value='#{partial_path}'>".html_safe
   end
   
+  ### remote partial helpers ###
+  
+  
+  
+  ### TEMPLATES & WIDGETS ###
+  # These methods render small bits of HTML based on parameters.
+  
+  # Renders a pretty 0-5 star rating bar
+  def rating_stars(n, opts={})
+    out_of = opts[:out_of] || 5
+    wrapper_class = opts[:wrapper_class] || ""
+    
+    html = "<div class='rating-stars #{wrapper_class}'>"
+    n.times { html << "<span class='glyphicon glyphicon-star'></span>" }
+    (out_of - n).times { html << "<span class='glyphicon glyphicon-star-empty text-muted'></span>" }
+    html << "</div>"
+    
+    html.html_safe
+  end
+  
   # Renders a bootstrap form group with static text
   def static_form_group(options={})
     ("<div class='form-group'>" +
@@ -50,19 +74,6 @@ module ApplicationHelper
         ) +
       "</div>" +
     "</div>").html_safe
-  end
-  
-  # Renders a pretty 0-5 star rating bar
-  def rating_stars(n, opts={})
-    out_of = opts[:out_of] || 5
-    wrapper_class = opts[:wrapper_class] || ""
-    
-    html = "<div class='rating-stars #{wrapper_class}'>"
-    n.times { html << "<span class='glyphicon glyphicon-star'></span>" }
-    (out_of - n).times { html << "<span class='glyphicon glyphicon-star-empty text-muted'></span>" }
-    html << "</div>"
-    
-    html.html_safe
   end
   
   # Renders a Logo Upload Form Element. Pass in a reference to the form builder object,
@@ -84,7 +95,28 @@ module ApplicationHelper
     html << "</div>"
     
     html.html_safe
+  end
+
+  # Renders a centered back link to the desired URL
+  def back_link(path, opts={})
+    link_label = opts[:label] || "Back"
     
+    html = "<div class='text-center'>"
+    html << link_to(link_label, path, method: :get, class: 'btn btn-lg btn-link')
+    html << "</div>"
+    
+    html.html_safe
+  end
+
+  ### templates & widgets ###
+  
+  
+  
+  ### MISCELLANEOUS HELPERS ###
+  
+  # Allows named yields within partial layouts
+  def yield_content(content_key)
+    view_flow.content.delete(content_key)
   end
   
   # Converts alert type to appropriate bootstrap class
@@ -95,5 +127,7 @@ module ApplicationHelper
     }
     alert_mappings[type.to_sym] || type.to_s
   end
+  
+  ### miscellaneous helpers ###
 
 end
