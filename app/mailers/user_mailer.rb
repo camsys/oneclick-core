a7d53937c0590122aee12edc3eedf628143cabe1
Nicolas Garcia <ngarcia@camsys.com>
2017-07-07 11:01:06 -0400

    Write spec for usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,5 @@
 class UserMailer < ApplicationMailer
   
-  def test_email(addr)
-    mail(to: addr, subject: "TEST EMAIL")
-  end
-  
   def agency_setup_reminder(agency)
     @agency = agency
     email_list = (agency.staff.pluck(:email) + [@agency.email] + User.admins.pluck(:email)).compact
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,9 +1,5 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-class UserMailerPreview < ActionMailer::Preview
-  def test_email_preview
-    UserMailer.test_email("nicolas.o.garcia@gmail.com")
-  end
-  
+class UserMailerPreview < ActionMailer::Preview  
   def agency_setup_reminder_preview
     UserMailer.agency_setup_reminder(PartnerAgency.last)
   end
