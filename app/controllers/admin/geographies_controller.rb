07d2818bdad4a1283dabe10d023675f964541d35
Nicolas Garcia <ngarcia@camsys.com>
2017-07-11 09:08:31 -0400

    Display errors for missing geometries

--- app/controllers/admin/geographies_controller.rb
+++ app/controllers/admin/geographies_controller.rb
@@ -6,6 +6,8 @@ class Admin::GeographiesController < Admin::AdminController
     @cities = City.all.order(:state, :name)
     @zipcodes = Zipcode.all.order(:name)
     @custom_geographies = CustomGeography.all.order(:name)
+    
+    check_for_missing_geometries(@counties, @cities, @zipcodes, @custom_geographies)
   end
 
   def upload_counties
@@ -52,6 +54,19 @@ class Admin::GeographiesController < Admin::AdminController
         render json: json_response
       end
     end
+  end
+  
+  protected
+  
+  def check_for_missing_geometries(*collections)
+    messages = []
+    collections.each do |collection|
+      missing_geoms = collection.where(geom: nil)
+      if missing_geoms.count > 0
+        messages << "The following #{collection.klass.name.titleize.pluralize} are missing geometry data and should be re-uploaded: #{missing_geoms.pluck(:name).join(', ')}"
+      end
+    end
+    flash[:warning] = messages.join("<br/>").html_safe unless messages.empty?
   end
 
 end
