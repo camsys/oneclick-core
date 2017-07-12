1b1ea597e0ceb50fa20211e82f5c3a1a5e496f3b
Nicolas Garcia <nicolas.o.garcia@gmail.com>
2017-07-12 16:09:59 -0400

    Agencies form improvements (#87)
    * Add Description Field to Agency Form
    
    * Validate email and phone with contactable module

--- app/controllers/api/v2/agencies_controller.rb
+++ app/controllers/api/v2/agencies_controller.rb
@@ -1,20 +1,28 @@
 module Api
   module V2
     class AgenciesController < ApiController
+      before_action :load_agencies
             
-      # Gets all agencies. An optional type parameter will filter by agency type
-      # (e.g. TransportationAgency, PartnerAgency)
+      # Gets all (published) agencies. An optional type parameter will filter
+      # by agency type (e.g. TransportationAgency, PartnerAgency)
       def index
 
         # Filter by Agency Type if type param is passed
         type_name = params[:type].to_s.strip.titleize + "Agency"
         type_name = Agency.agency_type_names.include?(type_name) ? type_name : nil
-        @agencies = type_name ? Agency.where(type: type_name) : Agency.all
+        @agencies = @agencies.where(type: type_name) if type_name
 
         render success_response(
             @agencies, 
             serializer: AgencySerializer, 
             root: "agencies")
+      end
+      
+      protected
+      
+      # Scope agencies to published only
+      def load_agencies
+        @agencies = Agency.published.order(:name)
       end
       
     end
