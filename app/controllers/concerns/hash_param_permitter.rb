288b8dd1a66a506216c5f4b051e24b7bd66481ef
Nicolas Garcia <ngarcia@camsys.com>
2017-08-04 15:39:01 -0400

    Fix bug in HashParamPermitter

--- app/controllers/concerns/hash_param_permitter.rb
+++ app/controllers/concerns/hash_param_permitter.rb
@@ -1,43 +1,44 @@
 # Service object for permitting complex hash params for serialized columns
+
+# HOW TO USE:
+# 1. Create a new class that inherits from HashParamPermitter
+# 2. Call the define_hash_structure method in the class body,
+#    passing the following options:
+#    hash_column: Symbol name of the column that contains the hash
+#    case_column: Symbol name of the column that determines which structure the hash should take
+#    structure: A hash with a key for each possible case_column name, and
+#      values which are arrays of each of the permitted params for that structure
+# 3. If needed, overwrite any permit_col_name methods with custom methods for more complex structures
+# 4. See FareParamPermitter or ServiceBookingParamPermitter for examples of how to implement
+
 class HashParamPermitter
   
-  attr_accessor :hash_column, :case_column, :structure
-
   # Pass the params object when initializing
   def initialize(params)
     @params = params
-    
-    # Set instance variables based on the class instance variables
-    @hash_column = self.class.hash_column
-    @case_column = self.class.case_column
-    @structure = self.class.structure
   end
-  
-  # Class Instance Variable Getters
-  def self.hash_column; @hash_column end
-  def self.case_column; @case_column end
-  def self.structure; @structure end
   
   # Config Method defines appropriate instance methods, 
   # which may be overwritten as needed
   def self.define_hash_structure(params={})
-    @hash_column = params[:hash_column]
-    @case_column = params[:case_column]
-    @structure = params[:structure]
-    
+    hash_column = params[:hash_column]
+    case_column = params[:case_column]
+    structure = params[:structure].with_indifferent_access # so strings and symbols both work
+
     # Defines top-level permit method
     define_method(:permit) do
+      
       # Return an empty array if the case column key is missing, or if it's an unknown key
-      return [] unless  @params.has_key?(self.case_column) ||
-                        self.structure.keys.include?(@params[self.case_column])
+      return [] unless  @params.has_key?(case_column) &&
+                        structure.keys.include?(@params[case_column])
 
       # Otherwise, return an array with the hash column as key and list of permitted sub-params as value
-      [self.hash_column => self.send("permit_#{@params[self.case_column]}")]
+      [hash_column => self.send("permit_#{@params[case_column]}")]
     end
     
     # Define a permit sub-method for each key in the structure
     # These methods may be overwritten by the inheriting class
-    @structure.each do |key, value|
+    structure.each do |key, value|
       define_method("permit_#{key}") do
         return value
       end
