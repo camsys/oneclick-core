b1593d4c4550909d834813986deeffb0713f6270
Nicolas Garcia <ngarcia@camsys.com>
2017-08-15 11:28:55 -0400

    Encrypt external user password on UserBookingProfile

--- app/models/user_booking_profile.rb
+++ app/models/user_booking_profile.rb
@@ -1,11 +1,15 @@
 class UserBookingProfile < ApplicationRecord
+
+  ### INCLUDES ###
+  include Encryptable
   
   ### ATTRIBUTES AND ASSOCIATIONS ###
   belongs_to :user
   belongs_to :service
   
   serialize :details
-  
+  encrypt_attribute :external_password, 'BOOKING_PASSWORD_ENCRYPTION_KEY' # Encryption key stored in ENV variable of this name
+
   
   ### INSTANCE METHODS ###
   
@@ -24,5 +28,5 @@ class UserBookingProfile < ApplicationRecord
   def authenticate?
     booking_ambassador.try(:authenticate_user?)
   end
-  
+    
 end
--- db/migrate/20170814204243_add_external_id_and_password_to_user_booking_profile.rb
+++ db/migrate/20170814204243_add_external_id_and_password_to_user_booking_profile.rb
@@ -1,9 +1,8 @@
 class AddExternalIdAndPasswordToUserBookingProfile < ActiveRecord::Migration[5.0]
   def change
-    change_table :user_booking_profiles do |t|
-      t.string :encrypted_external_password, null: false, default: ""
-      t.string :external_user_id, null: false, default: ""
-    end
+    add_column :user_booking_profiles, :encrypted_external_password, :string
+    add_column :user_booking_profiles, :encrypted_external_password_iv, :string # Needed for attr_encrypted
+    add_column :user_booking_profiles, :external_user_id, :string
     add_index :user_booking_profiles, :external_user_id
   end
 end
