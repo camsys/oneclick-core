98d754e78e870ae4590cb825fba2347ba6febec8
Derek Edwards <dedwards8@gmail.com>
2017-08-24 13:20:27 -0400

    Handle accommodation, elig, and purpose accommodations for v1

--- app/controllers/api/v1/translations_controller.rb
+++ app/controllers/api/v1/translations_controller.rb
@@ -35,15 +35,10 @@ module Api
           #The gsub finds all instances of %{xyz} and replaces then with {{xyz}} The {{xyz}} string is used by Angular for interpolation
           Translation.where(locale: locale).each {|translation| dictionary[translation.key] = translation.value.to_s.gsub(/%\{[a-zA-Z_]+\}/) { |s| '{{' + s[2..-2] + '}}' } }
 
-          eligs = dictionary.keys.select { |key| key.to_s.match(/^eligibility_/) }
-          eligs.each do |elig|
-            new_key = elig.gsub("eligibility_", "")
-            puts new_key
-            puts elig
-            dictionary[new_key] = dictionary[elig]
-          end
+          dictionary = add_v1_translations dictionary
 
           dictionaries = dictionary
+
         else
           Locale.all.each do |locale|
             dictionary = {} #Translation.where(locale: locale).map {|t| {t.key => t.value}}
@@ -56,6 +51,25 @@ module Api
 
         render status: 200, json: dictionaries
         return
+      end
+
+      # OCC uses translations like eligibilty_wheelchair_note, but V1 is looking for wheelchair_note.  
+      # Do this for all eligbilities, accommodations, and purposes
+      def add_v1_translations dictionary
+
+        ['eligibility', 'accommodation', 'purpose'].each do |trans_type|
+
+          trans = dictionary.keys.select { |key| key.to_s.match(/^#{trans_type}_/) }
+          trans.each do |tran|
+            new_key = tran.sub("#{trans_type}_", "")
+            puts new_key
+            puts trans
+            dictionary[new_key] = dictionary[trans]
+          end
+        end
+
+        dictionary
+
       end
 
     end
