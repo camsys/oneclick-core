679d33ec640090524ba0ff106ddd1e740d525a49
Derek Edwards <dedwards8@gmail.com>
2017-09-07 20:32:05 -0400

    Add basic model spec

--- spec/models/alert_spec.rb
+++ spec/models/alert_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Alert, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should respond_to :en_subject }
+  it { should respond_to :en_message }
 end
