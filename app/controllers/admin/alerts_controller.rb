e2e52e8b0ca63c5c58d0a560ce4bc7f120c8fd53
Derek Edwards <dedwards8@gmail.com>
2017-09-07 22:02:52 -0400

    another commit

--- app/controllers/admin/alerts_controller.rb
+++ app/controllers/admin/alerts_controller.rb
@@ -12,7 +12,17 @@ class Admin::AlertsController < Admin::AdminController
   end
 
   def create
+    puts params.ai 
+
   	@alert.update_attributes(alert_params)
+    
+    if @alert.audience == "specific_users"
+      puts params.ai 
+      puts params["audience_details"].ai 
+      puts alert_params.ai 
+      handle_specific_users alert_params["audience_details"]
+    end
+
     translations = params[:alert]
     translations.each do |translation, value|
       @alert.set_translation(translation.split('_').first, translation.split('_').last, value)
@@ -38,7 +48,14 @@ class Admin::AlertsController < Admin::AdminController
   private
 
   def alert_params
-  	params.require(:alert).permit(:expiration, :published)
+  	params.require(:alert).permit(:expiration, :published, :audience, audience_details: [:user_emails])
+  end
+
+  def handle_specific_users audience_details
+    audience_details["user_emails"].strip.split(',').each do |email|
+      puts email
+      puts 'DO SOMETHING HERE or move into the model'
+    end
   end
   
 end
