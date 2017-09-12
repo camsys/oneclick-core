1106f7a67ae069486d305b4f5990f9a5727d4e10
Derek Edwards <dedwards8@gmail.com>
2017-09-11 23:49:55 -0400

    Add service fare info to transit legs

--- app/serializers/api/v1/itinerary_serializer.rb
+++ app/serializers/api/v1/itinerary_serializer.rb
@@ -121,7 +121,7 @@ module Api
       end
 
       def json_legs
-        object.legs
+        augmented_legs
       end
 
       def logo_url
@@ -197,6 +197,17 @@ module Api
       
 
       private
+
+      def augmented_legs
+        if object.legs?
+          object.legs.each do |leg|
+            if leg['agencyId']
+              leg["serviceFareInfo"] = Service.find_by(gtfs_agency_id: leg['agencyId']).try(:url) 
+            end
+          end
+        end
+        object.legs
+      end
 
       def location_hash(waypoint)
         {
