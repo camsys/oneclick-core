d97d6b6390381add668baca9f28fdc762efc4abf
Derek Edwards <dedwards8@gmail.com>
2017-09-12 15:39:37 -0400

    Merge

--- app/controllers/api/v2/refernet/services_controller.rb
+++ app/controllers/api/v2/refernet/services_controller.rb
@@ -93,20 +93,12 @@ module Api
           plans = otp.multi_plan([requests])
 
           ### Unack the requests and build a hash of durations
-<<<<<<< HEAD
-          plans[:callback].each do |label, plan|
-            response = otp.unpack(plan.response)
-            puts response.ai 
-            itinerary = response.extract_itineraries.first
-            duration_hash[label] = itinerary.nil? ? nil : itinerary.itinerary["duration"]
-=======
           unless plans.nil? or plans[:callback].nil?
             plans[:callback].each do |label, plan|
               response = otp.unpack(plan.response)
               itinerary = response.extract_itineraries.first
               duration_hash[label] = itinerary.nil? ? nil : itinerary.itinerary["duration"]
             end
->>>>>>> 1.1.0
           end
           return duration_hash
         end
