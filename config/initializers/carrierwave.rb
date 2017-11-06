7dfd25ff446f6a54d309b5f1f58af1f76f0a7e00
Nicolas Garcia <ngarcia@camsys.com>
2017-11-06 09:57:02 -0500

    Fix syntax error in carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'] || "none",
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] || "none"
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] || "none",
       :region                 => ENV['S3_REGION'] || "us-east-1" # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = ENV['AWS_BUCKET'] || "none"
