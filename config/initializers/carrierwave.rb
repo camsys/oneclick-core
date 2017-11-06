7b3806b60400be4c9215f76bbc4174f4a8351db9
Nicolas Garcia <ngarcia@camsys.com>
2017-11-06 09:52:14 -0500

    Make Carrierwave's S3 region configurable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'] || "none",
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] || "none"
-      # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+      :region                 => ENV['S3_REGION'] || "us-east-1" # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = ENV['AWS_BUCKET'] || "none"
 end
