0c1842be55481fe88314a658f33f9c8b68bcfca7
Nicolas Garcia <ngarcia@camsys.com>
2017-11-09 10:12:46 -0500

    Configure admin & staff accounts to be lockable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024203532) do
+ActiveRecord::Schema.define(version: 20171109143829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -435,11 +435,15 @@ ActiveRecord::Schema.define(version: 20171024203532) do
     t.string   "last_name"
     t.integer  "preferred_locale_id"
     t.text     "preferred_trip_types"
+    t.integer  "failed_attempts",                   default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["last_name", "first_name"], name: "index_users_on_last_name_and_first_name", using: :btree
     t.index ["preferred_locale_id"], name: "index_users_on_preferred_locale_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
