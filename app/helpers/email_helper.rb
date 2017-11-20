a5aef88120d365dd9bc4de45c1e01c82cbdf5bcb
Nicolas Garcia <ngarcia@camsys.com>
2017-11-20 16:19:19 -0500

    Translate emails and namespace translations

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -13,15 +13,18 @@ module EmailHelper
 
     if hours > 0
       if hours > 1
-        time_string << hours.to_s + " #{translate('hours')} "
+        time_string << hours.to_s + " #{translate('global.time.hours')} "
       else
-        time_string << hours.to_s + " hour "
+        time_string << hours.to_s + " #{translate('global.time.hour')} "
       end
     end
 
     if minutes > 0 
-      time_string << minutes.to_s + " #{translate('minute')}"
-      time_string << "s" if minutes != 1
+      if minutes > 1
+        time_string << minutes.to_s + " #{translate('global.time.minutes')}"
+      else
+        time_string << minutes.to_s + " #{translate('global.time.minute')}"
+      end
     end
 
     time_string
@@ -33,30 +36,30 @@ module EmailHelper
     # convert the meters to miles
     miles = dist_in_meters * 0.000621371
     if miles < 0.1
-      dist_str = [(5280.0 * miles).round(0).to_s, translate("feet")].join(' ')
+      dist_str = [(5280.0 * miles).round(0).to_s, translate("global.distance.feet")].join(' ')
     else
-      dist_str = [miles.round(2).to_s, translate("miles")].join(' ')
+      dist_str = [miles.round(2).to_s, translate("global.distance.miles")].join(' ')
     end
 
     dist_str
   end
 
   def html_steps steps
-      html = ""
+    html = ""
 
-      steps.each do |hash|
-        html << "<p><b>"
-        html << translate(hash["relativeDirection"].downcase)
-        html << "</b> #{translate('onto')} "
-        html << hash["streetName"].to_s
-        html << ", "
-        html << exact_distance_to_words(hash["distance"] * 1.0)
-        html << "</br></p>"
-      end
-
-      html << "</div>"
-      return html.html_safe
+    steps.each do |hash|
+      html << "<p><b>"
+      html << translate("global.otp.relative_direction." + hash["relativeDirection"].downcase)
+      html << "</b> #{translate('global.otp.misc.onto')} "
+      html << hash["streetName"].to_s
+      html << ", "
+      html << exact_distance_to_words(hash["distance"] * 1.0)
+      html << "</br></p>"
     end
+
+    html << "</div>"
+    return html.html_safe
+  end
 
   # Returns a mode-specific icon
   def get_mode_icon mode
@@ -79,23 +82,23 @@ module EmailHelper
 
   end
 
-    # Returns a mode-specific icon
+  # Returns a mode-specific icon
   def short_description leg
 
     case leg['mode']
     when "WALK"
-      return "#{translate('Walk_to')} #{leg['to']['name']}"
+      return "#{translate('global.otp.misc.walk_to')} #{leg['to']['name']}"
     when "BICYCLE"
-      return "#{translate('Bike_to')} #{leg['to']['name']}"
+      return "#{translate('global.otp.misc.bike_to')} #{leg['to']['name']}"
     when "CAR"
-      return "#{translate('Drive_to')} #{leg['to']['name']}"
+      return "#{translate('global.otp.misc.drive_to')} #{leg['to']['name']}"
     when "TRAM", "SUBWAY"
-      return "#{leg['agencyName']} #{leg['route']} #{translate('to')} #{leg['to']['name']}"
+      return "#{leg['agencyName']} #{leg['route']} #{translate('global.otp.misc.to')} #{leg['to']['name']}"
     when "BUS"
-      return "#{leg['agencyName']} #{leg['route']} #{translate('to')} #{leg['to']['name']}"
+      return "#{leg['agencyName']} #{leg['route']} #{translate('global.otp.misc.to')} #{leg['to']['name']}"
     else 
       Rails.logger.info "#{leg['mode']} does not have a supported short description, defaulting to bus description"
-      return "#{leg['agencyName']} #{leg['route']} #{translate('to')} #{leg['to']['name']}"
+      return "#{leg['agencyName']} #{leg['route']} #{translate('global.otp.misc.to')} #{leg['to']['name']}"
     end
 
   end
