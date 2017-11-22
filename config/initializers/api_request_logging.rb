1347d223036586cae15ea1af7fdeb202b0aae0c1
Nicolas Garcia <ngarcia@camsys.com>
2017-11-22 11:55:11 -0500

    Make API Request Logging ENV configurable

--- config/initializers/api_request_logging.rb
+++ config/initializers/api_request_logging.rb
@@ -2,7 +2,12 @@
 # Creates an ApiRequestLogger, configured to log all requests to Api:: namespaced
 # controllers. May add controllers and/or specific actions to exclude, if desired.
 # See api_request_logger.rb for more details.
-ApiRequestLogger.new(
-  exclude_controllers: [],
-  exclude_actions: {}
-).start
+
+if(ENV['LOG_API_REQUESTS'] == "true")
+  puts "LOGGING API REQUESTS"
+  
+  ApiRequestLogger.new(
+    exclude_controllers: [],
+    exclude_actions: {}
+  ).start
+end
