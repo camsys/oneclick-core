827d7ad52e5c331762fee3421715ae3ad0ab5d4d
Nicolas Garcia <ngarcia@camsys.com>
2017-12-04 12:00:08 -0500

    Update 'this_week' to use beginning/end of day and proper time zones

--- config/initializers/date_time.rb
+++ config/initializers/date_time.rb
@@ -1,8 +1,9 @@
 class DateTime
 
-  # Returns a DateTime range of the past 7 days
+  # Returns a DateTime range of the past 7 days. 
+  # Goes from midnight (start of day) to midnight (end of day), covering a 7-day stretch
   def self.this_week
-    (DateTime.current - 7.days)..DateTime.current
+    (DateTime.current.in_time_zone.beginning_of_day - 6.days)..DateTime.current.in_time_zone.end_of_day
   end
   
 end
