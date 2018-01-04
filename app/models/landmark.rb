f52aa49374a0dac78b27c1225873ee178969a6bb
Taylor Liss <tliss@camsys.com>
2018-01-04 14:03:29 -0500

    Add validation for lat and lng

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -3,6 +3,8 @@ class Landmark < Place
   ### Validations ####
   validates :name, uniqueness: true
   validates :name, presence: true
+  validates :lat , numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }
+  validates :lng, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 
   #### Scopes ####
   scope :is_old, -> { where(:old => true) }
