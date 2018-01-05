882cd1070054170a43d365d887278a70cc206fc4
Derek Edwards <dedwards8@gmail.com>
2018-01-05 13:07:25 -0500

    A User can Sign up witha  specific OCC Server that books with Trapeze

--- app/services/external_api_ambassadors/trapeze_ambassador.rb
+++ app/services/external_api_ambassadors/trapeze_ambassador.rb
@@ -6,22 +6,38 @@ class TrapezeAmbassador < BookingAmbassador
   def initialize(opts={})
     super(opts)
     @url ||= Config.trapeze_url
-    @user ||= Config.trapeze_user
-    @token ||= Config.trapeze_token
-    @client = create_client(Config.trapeze_url, Config.trapeze_url, @user, @token)
-    @client_id = opts[:client_id]
-    @client_password = opts[:client_password]
+    @api_user ||= Config.trapeze_user
+    @api_token ||= Config.trapeze_token
+    @client = create_client(Config.trapeze_url, Config.trapeze_url, @api_user, @api_token)
   end
 
+  #####################################################################
+  ## Top-Level Required Methods in order for BookingAmbassador to work
+  #####################################################################
   # Returns symbol for identifying booking api type
   def booking_api
     :trapeze
   end
 
+  # Used to test if a Oneclick Service is setup correctly
+  def authentic_provider?
+    #TODO: Call Trapeze to Confirm that the ProviderID exists before allowing a Service to set it's ID
+    true
+  end
+
+  # Returns True if a User is a valid Trapeze User
+  def authenticate_user?
+    pass_validate_client_password
+  end
+
+  #####################################################################
+  ## SOAP Calls to Trapeze
+  #####################################################################
   def pass_validate_client_password
     begin
-      response = @client.call(:pass_validate_client_password, message: {client_id: @client_id, password: @client_password})
-    rescue
+      response = @client.call(:pass_validate_client_password, message: {client_id: customer_id, password: customer_token})
+    rescue => e
+      Rails.logger.error e.message 
       return false
     end
 
@@ -34,25 +50,32 @@ class TrapezeAmbassador < BookingAmbassador
     end
   end
 
-  def authentic_provider?
-    #TODO: Call Trapeze to Confirm that the ProviderID exists before allowing a Service to set it's ID
-    true
+  #####################################################################
+  ## Helper Methods
+  #####################################################################
+  # Gets the customer id from the user's booking profile
+  def customer_id
+    @booking_profile.try(:external_user_id)
+  end
+
+  # Gets the customer token from the user's booking profile  
+  def customer_token
+    @booking_profile.try(:external_password)
   end
 
   protected
 
   # Create a Client
   def create_client(endpoint, namespace, username, password)
-
     client = Savon.client do
       endpoint endpoint
       namespace namespace
       basic_auth [username, password]
       convert_request_keys_to :camelcase
     end
-
     client
-
   end
+
+
 
 end
