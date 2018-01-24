dfc9729ac32d54669b1fb6d611913f248a754551
Taylor A. Liss <tayloraliss@gmail.com>
2018-01-24 09:57:08 -0500

    Landmark error message (#154)
    OCC-233
    
    * Add detailed error logging for uploaded .csv file
    
    * Simplify validation failure reporting for landmarks
    
    * Remove unnecessary variables

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -38,9 +38,9 @@ class Landmark < Place
         	  lng: row[7],
             old: false
           })
-        rescue
+        rescue Exception => msg
           #Found an error, back out all changes and restore previous POIs
-          message = 'Error found on line: ' + line.to_s
+          message = 'Error on line ' + line.to_s + ' of .csv file - ' + msg.to_s
           Rails.logger.info message
           Rails.logger.info 'All changes have been rolled-back and previous Landmarks have been restored'
           Landmark.is_new.delete_all
