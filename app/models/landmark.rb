9f326ac437ac569d77c0316501bddb8c76fc8f53
Taylor A. Liss <tayloraliss@gmail.com>
2018-01-24 17:18:30 -0500

    Revert "Landmark error message (#156)"
    This reverts commit 957cc69e0e265022ee4270011d2dcb647d7c7e4c.

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -23,10 +23,8 @@ class Landmark < Place
     message = ""
     Landmark.update_all(old: true)
     line = 2 #Line 1 is the header, start with line 2 in the count
-    numberOfLines = 0 #Incremented once for every line in CSV
     begin
       CSV.foreach(landmarks_file, {:col_sep => ",", :headers => true}) do |row|
-        numberOfLines += 1
         begin
           #If we have already created this Landmark, don't create it again.
           l = Landmark.create!({
@@ -40,9 +38,9 @@ class Landmark < Place
         	  lng: row[7],
             old: false
           })
-        rescue Exception => msg
+        rescue
           #Found an error, back out all changes and restore previous POIs
-          message = 'Error on line ' + line.to_s + ' of .csv file - ' + msg.to_s
+          message = 'Error found on line: ' + line.to_s
           Rails.logger.info message
           Rails.logger.info 'All changes have been rolled-back and previous Landmarks have been restored'
           Landmark.is_new.delete_all
@@ -65,7 +63,8 @@ class Landmark < Place
     if failed
       return false, message
     else
-      return true, numberOfLines.to_s + " landmarks loaded"
+      Landmark.is_old.delete_all
+      return true, Landmark.count.to_s + " landmarks loaded"
     end
 
   end #Update
