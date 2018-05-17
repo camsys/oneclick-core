6031d6dddb983cdd8187c9eb70641e26dda9de88
Derek Edwards <dedwards8@gmail.com>
2018-05-17 10:55:45 -0400

    Fix broken places call when name is nil.

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -12,20 +12,18 @@ module Api
 
         #If the search string is empty, just return the recent places for the user.
         if search_string == "% %"
-          count = 0
-          recent_places = authentication_successful? ? @traveler.recent_waypoints(max_results) : []
+          recent_places = authentication_successful? ? @traveler.recent_waypoints(3*max_results) : []
           recent_places.each do |landmark|
             landmark_hash = landmark.google_place_hash
             ["id"].each do |key|
               landmark_hash.delete(key)
             end
-            if landmark_hash["name"].in? landmark_hash["formatted_address"]
+            if (landmark_hash["name"] || "").in? landmark_hash["formatted_address"]
               landmark_hash["name"] =  ""
             end
             locations.append(landmark_hash)
             locations.uniq!
-            count +=1 
-            if count >= max_results
+            if locations.count >= max_results
               break
             end
           end
