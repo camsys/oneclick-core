9327fbd5ce71a1ddf3291dc98071d7f28834e899
Derek Edwards <dedwards8@gmail.com>
2018-07-11 16:25:55 -0400

    Update spec for GoogleAPIKey

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,4 +41,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # FAKED OUT ENV VARIABLES
+  ENV["GOOGLE_API_KEY"] = "FAKEGOOGLEKEY"
+
 end
