c7c10d0a305dd348affece49457325c18c37275f
Derek Edwards <dedwards8@gmail.com>
2019-03-10 17:52:15 -0400

    Send back real status.

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -312,7 +312,7 @@ module Api
             json_legs: itinerary.legs,
             mode: itinerary.trip_type.nil? ? nil : remodeify(itinerary.trip_type),
             product_id: nil, #itinerary.product_id,
-            status: "active", # DEPRECATE?
+            status: itinerary.booking.try(:status) || nil, # DEPRECATE?
             transfers: nil, #itinerary.transfers, # DEPRECATE?
             transit_time: itinerary.transit_time,
             wait_time: nil, #itinerary.wait_time, # WAIT TIME?
