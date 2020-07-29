ee9e60d7fc3439e08e707ad40bbbcfc3ff68e2a1
tmahtab <tmahtab@camsys.com>
2020-07-29 16:01:33 -0700

    Added missing refernet_category_id column

--- db/migrate/20200729200521_add_taxonomy_code_sub_sub_categories.oneclick_refernet.rb
+++ db/migrate/20200729200521_add_taxonomy_code_sub_sub_categories.oneclick_refernet.rb
@@ -3,7 +3,7 @@ class AddTaxonomyCodeSubSubCategories < ActiveRecord::Migration[5.0]
   def change
     add_column :oneclick_refernet_sub_sub_categories, :taxonomy_code, :string
 
-    change_column :oneclick_refernet_sub_sub_categories,  :refernet_category_id, :string
+    add_column :oneclick_refernet_sub_sub_categories,  :refernet_category_id, :string
     change_column :oneclick_refernet_services, :refernet_service_id, :string
     change_column :oneclick_refernet_services, :refernet_location_id, :string
     change_column :oneclick_refernet_services, :refernet_servicesite_id, :string
