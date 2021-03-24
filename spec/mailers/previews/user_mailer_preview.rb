30e62e26021c6f389422426f0416779fbc586da7
Wilson Jiang <wilsonj806@gmail.com>
2021-03-24 15:47:16 -0400

    Add Email Preview for trip confirmation emails

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -3,4 +3,13 @@ class UserMailerPreview < ActionMailer::Preview
   def agency_setup_reminder_preview
     UserMailer.agency_setup_reminder(PartnerAgency.last)
   end
+
+  def user_trip_email
+    UserMailer.user_trip_email(User.find(2), Trip.find(70))
+  end
+
+  def ecolane_trip_email
+    UserMailer.ecolane_trip_email('wjiang@camsys.com', Booking.limit(5))
+  end
+
 end
