9b19cace223790f3a2675ae3d25d2f6ce49f120e
Wilson Jiang <wilsonj806@gmail.com>
2021-05-11 13:40:33 -0400

    OCC-556 feat: add phi modification logging for database operations done thru Rails

---
+++ config/initializers/log_db_changes.rb
@@ -0,0 +1,15 @@
+require_relative '../../lib/modules/logging_helper'
+
+if ENV['RAILS_LOG_TO_STDOUT']
+  ActiveSupport::Notifications
+    .subscribe("sql.active_record") do |name, start, finish, id, payload|
+    if /^(CREATE[[:blank:]]TABLE|ALTER[[:blank:]]TABLE|DROP[[:blank:]]TABLE)/i.match(payload[:sql])
+      json = {
+        data_access_type: 'PHI_MODIFICATION',
+        **payload,
+        timestamp: Time.now,
+      }
+      Rails.application.config.phi_logger.info(JSON::dump(json))
+    end
+  end
+end
