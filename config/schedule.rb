8f12c09f0f6e59829f17752b731db0a11617699f
W. Scott Meeks <smeeks@camsys.com>
2021-05-12 14:24:42 -0400

    PAMF-667 Tweak times for UTC

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,11 +20,12 @@
 
 set :output, "/home/ubuntu/oneclick-core/log/cron_log.log"
 
+# Times are UTC
 every 1.day, at: '7:00 am' do
   rake 'scheduled:daily'
 end
 
-every 1.day, at: '2:00 am' do
+every 1.day, at: '5:00 am' do
   rake 'ecolane:update_pois'
 end
 
