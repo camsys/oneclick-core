50c1068fc59a40bfe78ce226c7a17fa8c3ac3cbe
Wilson Jiang <wilsonj806@gmail.com>
2021-05-17 11:43:06 -0400

    OCC-556 refactor: add a shorter way to identify what the auth failure is

--- config/initializers/warden_config.rb
+++ config/initializers/warden_config.rb
@@ -1,14 +1,11 @@
 require_relative '../../lib/modules/logging_helper'
 
 Warden::Manager.before_failure do |env, opts|
-  puts opts
-  puts opts[:message]
-  puts opts[:message].class
-  # log auth err here
-  # add timestamp
-  # add better comment for log
+  # Add logging for authentication failure,
+  #
   json = {
     data_access_type: "PHI_AUTH_FAILURE",
+    message_tag: opts[:message],
     **opts,
     message: LoggingHelper::WARDEN_FAILURE_MESSAGES[opts[:message]],
     timestamp: Time.now
