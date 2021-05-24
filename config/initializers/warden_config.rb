f43991e0678fc2d3b7ea4bd54e065aef1ebeb465
Wilson Jiang <wilsonj806@gmail.com>
2021-05-24 16:29:35 -0400

    OCC-556 refactor: remove custom message from auth failure logging and use opts[:message] instead

--- config/initializers/warden_config.rb
+++ config/initializers/warden_config.rb
@@ -21,9 +21,8 @@ Warden::Manager.before_failure do |env, opts|
     data_access_type: "PHI_AUTH_FAILURE",
     user_role: user_role,
     user_id: user_id,
-    message_tag: opts[:message],
+    message: opts[:message],
     **opts,
-    message: LoggingHelper::WARDEN_FAILURE_MESSAGES[opts[:message]],
     timestamp: Time.now
   }
   Rails.application.config.phi_logger.info(JSON::dump(json))
