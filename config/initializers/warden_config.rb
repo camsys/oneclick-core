184d2fbe7fc46ea28df123a126373db49acc3a7b
Wilson Jiang <wilsonj806@gmail.com>
2021-06-03 16:47:47 -0400

    feat: add logging for users logging out

--- config/initializers/warden_config.rb
+++ config/initializers/warden_config.rb
@@ -57,3 +57,29 @@ Warden::Manager.after_authentication except: :fetch do |user, auth, opts|
   }
   Rails.application.config.logger.info(JSON::dump(json))
 end
+
+Warden::Manager.before_logout do |user,auth,opts|
+  # Adds logging for authentication success
+  user_role = nil
+  auth_user = User.find_by(email: auth.env["HTTP_X_USER_EMAIL"])
+  if auth_user.admin?
+    user_role = :admin
+  elsif auth_user.staff?
+    user_role = :staff
+  else
+    user_role = :traveler
+  end
+  accessing_ip = auth.env["REMOTE_ADDR"]
+  origin = auth.env["HTTP_ORIGIN"]
+  json = {
+    data_access_type: "PHI_AUTH_SESSION_DESTROYED",
+    user_role: user_role,
+    user_id: auth_user.id,
+    accessing_ip: accessing_ip,
+    origin: origin,
+    message: opts[:event],
+    **opts,
+    timestamp: Time.now
+  }
+  Rails.application.config.logger.info(JSON::dump(json))
+end
