d5bf0341ae3bfce9a86e92b222227badfa211edf
Wilson Jiang <wilsonj806@gmail.com>
2021-06-04 12:08:15 -0400

    feat: add user id to standard log events

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -10,11 +10,13 @@ Rails.application.configure do
   config.lograge.custom_options = lambda do |event|
     head= !event.payload[:headers].nil? ? event.payload[:headers] : nil
     origin = !head.nil? ? head["HTTP_ORIGIN"] : nil
+    user = User.find_by(email:!head.nil? ? head["HTTP_X_USER_EMAIL"] : nil)
     ip = !head.nil? ? head["REMOTE_ADDR"] : nil
     data_access_type = LoggingHelper::check_if_phi(event.payload)
     status = LoggingHelper::check_if_devise_sign_in(event.payload)
     {
       :params => event.payload[:params],
+      :user_id => user&.id,
       :data_access_type => data_access_type,
       :timestamp => Time.now,
       :log_level => LoggingHelper::return_log_level(status),
