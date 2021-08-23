f16e640119756db9122e3eff6ec56f10c47cb9ad
Wilson Jiang <wilsonj806@gmail.com>
2021-08-23 16:27:43 -0400

    Update permissions for staff, admin, and unaffiliated staff/ admin users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,9 +19,13 @@ class Ability
     end
     
     
-    ### STAFF PERSMISSIONS ###
+    ### STAFF PERMISSIONS ###
     if user.staff?
-
+      # If a staff user has no agency, then they can only update themselves
+      if user.staff_agency.nil?
+        can :update, User, id: user.id
+        return
+      end
       ## General Staff Permissions ##
       can [:read, :update], Agency,     # Can read or update their own agency
         id: user.staff_agency.try(:id)
@@ -48,7 +52,24 @@ class Ability
     
     ### ADMIN PERMISSIONS ###
     if user.admin?
-      can :manage, :all # Can perform all actions on all models
+      if user.staff_agency.nil?
+        can :update, User, id: user.id
+        return
+      end
+      can [:read, :update], Agency,     # Can read or update their own agency
+          id: user.staff_agency.try(:id)
+      can :manage, User,                # Can manage users that are staff for the same agency
+          id: user.accessible_staff.pluck(:id)
+      can :manage, Service,             # Can CRUD services under their agency
+          id: user.services.pluck(:id)
+      can :create, Service              # Can create new services
+      can :manage, Alert
+      if user.oversight_admin?
+        # Oversight Admins cannot manage superusers
+        cannot :manage, User,           # Cannot manage superusers
+           id: User.all.superuser.pluck(:id)
+        # TODO: Add the rest of the oversight admin's permissions
+      end
     end
 
     ### SUPERUSER PERMISSIONS ###
