765bc9cbac5612d02e3fb4dc8f091d2d5f425afc
Wilson Jiang <wilsonj806@gmail.com>
2021-08-30 11:05:43 -0400

    update abilities to match part of(or hopefully most of/ all of) the requirements

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -26,21 +26,23 @@ class Ability
         can :update, User, id: user.id
         return
       end
+
       ## General Staff Permissions ##
       can [:read, :update], Agency,     # Can read or update their own agency
         id: user.staff_agency.try(:id)
       can :read, User,                # Can manage users that are staff for the same agency
         id: user.accessible_staff.pluck(:id)
-      can :manage, Service,             # Can CRUD services under their agency
+      can :read, Service,              # Can read services under that user
         id: user.services.pluck(:id)
-      can :create, Service              # Can create new services
       can :manage, Alert                # Can manage alerts
-      
+      can :read, :report         # Can read reports
+
+
       ## TransportationAgency Staff Permissions ##
       if user.transportation_staff?
         can [:read, :update], Feedback, # Can read/update feedbacks related to their agency's services
           id: Feedback.about(user.services).pluck(:id)
-        can [:create], User
+        can [:read,:create], User
       end
       
       ## PartnerAgency Staff Permissions ##
@@ -52,7 +54,6 @@ class Ability
       ## OversightAgency Staff Permissions ##
       if user.oversight_staff?
         can [:read, :update], Feedback  # Can read/update ALL feedbacks
-        can :read, :report              # Can view all reports
       end
       
     end # staff
@@ -63,28 +64,34 @@ class Ability
         can :update, User, id: user.id
         return
       end
+
+      # General affiliated admin permissions
       can [:read, :update], Agency,     # Can read or update their own agency
           id: user.staff_agency.try(:id)
-      can :manage, User,                # Can manage users that are staff for the same agency
-          id: user.accessible_staff.pluck(:id)
+      can :manage, User,                # Can manage users that are staff for the same agency or unaffiliated staff
+          id: user.accessible_staff.pluck(:id).concat(User.staff_for_none,User.admin_for_none)
       can :manage, Service,             # Can CRUD services under their agency
           id: user.services.pluck(:id)
       can :create, Service              # Can create new services
       can :manage, Alert
+      can :read, :report         # Can view all reports
+      # Mapping related permissions
+      can :create, GeographyRecord      # Can create Geography records
+
+      # Oversight Admin Permissions
       if user.oversight_admin?                # Can manage Transportation Agencies assigned to the user's Oveersight Agency
         can :manage, Agency,
             id: user.staff_agency.agency_oversight_agency.pluck(:transportation_agency_id)
         can :manage, Role               # Can manage Roles
-
+        # Mapping related permissions
+        can :manage, GeographyRecord    # Can manage geography records
         # Oversight Admins cannot manage superusers
         cannot :manage, User,           # Cannot manage superusers
            id: User.all.superuser.pluck(:id)
         cannot :manage, Role,               # Cannot manage superuser Roles
                id: Role.find_by(name: :superuser)
-
-        # TODO: Add the rest of the oversight admin's permissions
       end
-    end
+    end # end admin
 
     ### SUPERUSER PERMISSIONS ###
     if user.superuser?
