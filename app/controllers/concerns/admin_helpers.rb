0477501c05bf76b60bb8495a73ef738aab9b34a3
Wilson Jiang <wilsonj806@gmail.com>
2022-01-11 09:59:04 -0500

    OCC-697 OCC-698 OCC-699 update links available in the main dashboard page and utilities dropdown

--- app/controllers/concerns/admin_helpers.rb
+++ app/controllers/concerns/admin_helpers.rb
@@ -2,17 +2,9 @@ module AdminHelpers
 
   def get_admin_pages
     urls = Rails.application.routes.url_helpers
-
-    @admin_pages = [
-      { label: "Accommodations",  url: urls.admin_accommodations_path,  show: can?(:read, Accommodation) },
-      { label: "Alerts",          url: urls.admin_alerts_path,          show: can?(:read, Alert) },
+    global_pages = [
       { label: "Agencies",        url: urls.admin_agencies_path,        show: can?(:read, Agency) },
       { label: "Configuration",   url: urls.admin_configs_path,         show: can?(:read, Config) },
-      { label: "Feedback",        url: urls.admin_feedbacks_path,       show: can?(:read, Feedback) },
-      { label: "Eligibilities",   url: urls.admin_eligibilities_path,   show: can?(:read, Eligibility) },
-      { label: "Geography",       url: urls.admin_geographies_path,     show: can?(:read, GeographyRecord) },
-      { label: "Landmarks",       url: urls.admin_landmarks_path,       show: can?(:read, Landmark) },
-      { label: "Purposes",        url: urls.admin_purposes_path,        show: can?(:read, Purpose) },
       { label: "Reports",         url: urls.admin_reports_path,         show: can?(:read, :report) },
       { label: "Services",        url: urls.admin_services_path,        show: can?(:read, Service) },
       { label: "Staff",           url: urls.staff_admin_users_path,           show: can?(:read, User) },
@@ -21,8 +13,29 @@ module AdminHelpers
       { label: "My Agency",
         url: current_user.staff_agency.present? ? urls.admin_agency_path(current_user.staff_agency.try(:id)) : "",
         show: current_user.staff_agency.present? }
-    ].select {|page| page[:show] }
-    .sort_by { |page| page[:label] }
+    ]
+
+    @admin_pages = global_pages.concat(return_pages_by_mode)
+                               .select {|page| page[:show] }
+                               .sort_by { |page| page[:label] }
+
+  end
+
+  def return_pages_by_mode
+    urls = Rails.application.routes.url_helpers
+    mode = Config.find_by(key: :dashboard_mode).try(:value).to_sym
+    mode == :travel_patterns ? [
+      { label: "Travel Patterns", url: urls.admin_travel_patterns_path, show: can?(:read, TravelPattern) }
+    ] :
+      [
+        { label: "Accommodations",  url: urls.admin_accommodations_path,  show: can?(:read, Accommodation) },
+        { label: "Alerts",          url: urls.admin_alerts_path,          show: can?(:read, Alert) },
+        { label: "Eligibilities",   url: urls.admin_eligibilities_path,   show: can?(:read, Eligibility) },
+        { label: "Feedback",        url: urls.admin_feedbacks_path,       show: can?(:read, Feedback) },
+        { label: "Geography",       url: urls.admin_geographies_path,     show: can?(:read, GeographyRecord) },
+        { label: "Landmarks",       url: urls.admin_landmarks_path,       show: can?(:read, Landmark) },
+        { label: "Purposes",        url: urls.admin_purposes_path,        show: can?(:read, Purpose) },
+      ]
   end
 
 end
