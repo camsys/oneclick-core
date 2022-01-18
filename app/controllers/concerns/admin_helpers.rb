bf147bc5edd9ec0aee5403e4babf625c8d2c8870
Wilson Jiang <wilsonj806@gmail.com>
2022-01-18 13:45:35 -0500

    add fallback for getting pages based on dashboard mode

--- app/controllers/concerns/admin_helpers.rb
+++ app/controllers/concerns/admin_helpers.rb
@@ -23,7 +23,7 @@ module AdminHelpers
 
   def return_pages_by_mode
     urls = Rails.application.routes.url_helpers
-    mode = Config.find_by(key: :dashboard_mode).try(:value).to_sym
+    mode = Config.find_by(key: :dashboard_mode).try(:value)&.to_sym || :default
     mode == :travel_patterns ? [
       { label: "Travel Patterns", url: urls.required_records_admin_travel_patterns_path, show: can?(:read, TravelPattern) }
     ] :
