ab2dee2b16894fd87ba3c2d95df2ea7dd6992f4b
Wilson Jiang <wilsonj806@gmail.com>
2022-01-24 09:27:19 -0500

    skip travel patterns controller tests for now

--- spec/controllers/admin/travel_patterns_controller_spec.rb
+++ spec/controllers/admin/travel_patterns_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Admin::TravelPatternsController, type: :controller do
     end
   end
 
-  describe "POST #create" do
+  xdescribe "POST #create" do
     it "returns http success" do
       post :create
       expect(response).to have_http_status(:success)
