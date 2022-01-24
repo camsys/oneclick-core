bd72279ee003958839506711ee24dc6ebab8e222
Wilson Jiang <wilsonj806@gmail.com>
2022-01-24 13:44:14 -0500

    remove tests in travel patterns controller until implemented

--- spec/controllers/admin/travel_patterns_controller_spec.rb
+++ spec/controllers/admin/travel_patterns_controller_spec.rb
@@ -1,19 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Admin::TravelPatternsController, type: :controller do
-
-  xdescribe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  xdescribe "POST #create" do
-    it "returns http success" do
-      post :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
