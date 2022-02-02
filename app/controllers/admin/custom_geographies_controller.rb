5e5bab270a4d7745a91a65aae0eae22cf33ba3e7
Wilson Jiang <wilsonj806@gmail.com>
2022-02-02 10:46:14 -0500

    add quick custom geography delete handling

--- app/controllers/admin/custom_geographies_controller.rb
+++ app/controllers/admin/custom_geographies_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CustomGeographiesController < Admin::AdminController
-  authorize_resource :geography_record, parent: false
+  load_and_authorize_resource
 
   def index
     @geographies = current_user.get_geographies_for_user
@@ -18,11 +18,12 @@ class Admin::CustomGeographiesController < Admin::AdminController
     redirect_to admin_custom_geographies_path(uploader.errors.empty? ? { selected: uploader.custom_geo } : {})
   end
 
-  private
-  def custom_geography_params
-    params.require(:custom_geography).permit(
-      :agency,
-      :id
-    )
+  def destroy
+    if @custom_geography.destroy
+      flash[:success] = "Custom Geography Deleted Successfully"
+    else
+      flash[:warning] = "Custom Geography could not be Deleted"
+    end
+    redirect_back(fallback_location: admin_path)
   end
 end
