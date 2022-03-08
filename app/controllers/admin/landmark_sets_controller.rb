2844ee9ad3bb7800fbb8f7cf596d368831b91cc4
Wilson <wilsonj806@gmail.com>
2022-03-08 15:03:25 -0500

    Add progress for Create New Sets of POI page work

--- app/controllers/admin/landmark_sets_controller.rb
+++ app/controllers/admin/landmark_sets_controller.rb
@@ -4,5 +4,25 @@ class Admin::LandmarkSetsController < Admin::AdminController
   end
 
   def create
+    create_params = landmark_set_params
+    id = create_params[:agency_id]
+    agency = Agency.find id
+    # NOTE: the below probably could be cleaned up a bit better or turned into a custom validator for agency
+    if agency.nil?
+      flash[:error] = 'agency cannot be blank'
+      return redirect_to admin_landmark_sets_path
+    end
+    LandmarkSet.create(create_params)
+    redirect_to admin_landmark_sets_path
   end
+
+  private
+  def landmark_set_params
+    params.require(:landmark_set).permit([
+      :name,
+      :description,
+      :agency_id
+     ])
+  end
+
 end
