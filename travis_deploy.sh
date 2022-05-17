5818cdbb2a0cb1a9abf7a9e85cde1efe5620fa92
W. Scott Meeks <smeeks@camsys.com>
2022-05-17 14:37:35 -0400

    OCC-757 skip cleanup to leave pem file in place
    Also skipping unit tests temporarily to speed testing

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -1,5 +1,5 @@
 eval "$(ssh-agent -s)" #start the ssh agent
-ssh-add ${TRAVIS_BUILD_DIR}/cpta_aws.pem
+ssh-add cpta_aws.pem
 git remote add deploy "ssh://ubuntu@occ-pa-dev-ec2.camsys-apps.com:/home/ubuntu/oneclick-core"
 git push deploy develop
 
