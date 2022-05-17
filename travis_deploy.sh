64ec351403c6d13789960a0e4f215d893c257307
W. Scott Meeks <smeeks@camsys.com>
2022-05-17 14:53:25 -0400

    OCC-757 avoid answering "Are you sure you want to continue connecting?"

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -1,6 +1,6 @@
 eval "$(ssh-agent -s)" #start the ssh agent
 ssh-add cpta_aws.pem
 git remote add deploy "ssh://ubuntu@occ-pa-dev-ec2.camsys-apps.com:/home/ubuntu/oneclick-core"
-git push deploy develop
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push deploy develop
 
 
