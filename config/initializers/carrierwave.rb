0e6a0508d9c919f71823e8253f6788254b6efff0
tmahtab <tmahtab@camsys.com>
2022-06-03 17:34:29 -0700

    Removed provider config for server environment.(PAMF-772)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,5 @@
 CarrierWave.configure do |config|
   config.aws_credentials = {
-      :provider               => 'AWS',
       :region                 => ENV['S3_REGION'] || "us-east-1" # Change this for different AWS region. Default is 'us-east-1'
   }
   config.aws_bucket  = ENV['AWS_BUCKET'] || "none"
