635751204458d7396db227f33888ab1634848ec9
Drew Teter <dteter@camsys.com>
2022-09-12 05:18:20 -0400

    OCC-975

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -7,6 +7,8 @@ class Purpose < ApplicationRecord
   belongs_to :agency
   has_many :trips
   has_and_belongs_to_many :services
+  has_many :travel_pattern_purposes
+  has_many :travel_patterns, through: :travel_pattern_purposes, dependent: :restrict_with_error
 
   before_save :snake_casify, if: :has_code?
   validate :name_is_present?
