8c797b5dee1480d1060f11c09e4ab5e19bd70d0e
tmahtab <tmahtab@camsys.com>
2022-11-21 17:56:56 -0800

    Prevent deletion if in use by a region.(OCC-1071)

--- app/models/custom_geography.rb
+++ app/models/custom_geography.rb
@@ -7,6 +7,20 @@ class CustomGeography < GeographyRecord
   acts_as_geo_ingredient attributes: [:name, :buffer]
   belongs_to :agency
 
+  # Prevent deletion if in use by a region.
+  def destroy
+    regions = Region.all
+    regions.each do |region|
+      region.recipe.ingredients.each do |ingredient|
+        if ingredient.model.to_s == CustomGeography.name && self.name == ingredient.attributes[:name]
+          return false
+        end
+      end
+    end
+
+    super
+  end
+
   # Returns a GeoIngredient refering to this custom geography
   def to_geo
     GeoIngredient.new('CustomGeography', name: name, buffer: 0)
