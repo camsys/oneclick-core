9a9372cc362fa062dbe3b262f2d1a7fbb6fbc3b1
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2023-07-17 10:05:07 -0500

    wip

--- app/services/external_api_ambassadors/otp_ambassador.rb
+++ app/services/external_api_ambassadors/otp_ambassador.rb
@@ -76,9 +76,9 @@ class OTPAmbassador
   def get_itineraries(trip_type)
     return [] if errors(trip_type)
     itineraries = ensure_response(trip_type).itineraries
-    itineraries = filter_itineraries_by_walk_distance(itineraries, trip_type)
+    itineraries = filter_itineraries_by_walk_distance(itineraries)
     return itineraries.map {|i| convert_itinerary(i, trip_type)}.compact
-  end
+  end  
   
   # Filters out itineraries where any walking leg exceeds the maximum walk distance
   def filter_itineraries_by_walk_distance(itineraries)
