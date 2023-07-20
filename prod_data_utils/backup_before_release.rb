b5bc9dca1ab3fd6f5448c3f02c5fb3a8c80b994a
W. Scott Meeks <smeeks@camsys.com>
2023-07-20 11:09:27 -0400

    FMRPA-114 1-Click includes v in version

--- prod_data_utils/backup_before_release.rb
+++ prod_data_utils/backup_before_release.rb
@@ -17,11 +17,11 @@ logger = Logger.new("prod_data_check/logs/backup_#{version}.log", "daily")
 logger.level = Logger::DEBUG
 
 logger.info "-------------------------------------------------------------------------------------------------------------"
-logger.info "---------------- start backup for #{app} v#{version} on #{DateTime.now.to_s} ------------------"
+logger.info "---------------- start backup for #{app} #{version} on #{DateTime.now.to_s} ------------------"
 
 require_relative "db_backup_utils"
 db_util = DbBackupUtils.new(config, version, ENV['RAILS_ENV'], logger: logger)
 db_util.run
 
-logger.info "---------------- finished backup for #{app} v#{version} on #{DateTime.now.to_s} ------------------"
+logger.info "---------------- finished backup for #{app} #{version} on #{DateTime.now.to_s} ------------------"
 logger.info "--------------------------------------------------------------------------------------------------------------"
--- prod_data_utils/run.rb
+++ prod_data_utils/run.rb
@@ -23,7 +23,7 @@ logger = Logger.new("prod_data_utils/logs/prod_data_utils_#{version}.log", "dail
 logger.level = Logger::DEBUG 
 
 logger.info "-------------------------------------------------------------------------------------------------------------"
-logger.info "---------------- start data check for #{app} v#{version} on #{DateTime.now.to_s} ------------------"
+logger.info "---------------- start data check for #{app} #{version} on #{DateTime.now.to_s} ------------------"
 
 if version == Rails.application.config.version
   puts 'We are creating a db replica of production'
@@ -38,6 +38,6 @@ else
   puts 'Not creating a db replica of production'
 end
 
-logger.info "---------------- finish data check for #{app} v#{version} on #{DateTime.now.to_s} ------------------"
+logger.info "---------------- finish data check for #{app} #{version} on #{DateTime.now.to_s} ------------------"
 logger.info "--------------------------------------------------------------------------------------------------------------"
 
