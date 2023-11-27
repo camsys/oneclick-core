bfecca983eaa8626a18b4d50dad4c205f9551038
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2023-11-27 15:56:55 -0600

    init

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -104,8 +104,8 @@ module Api
       
             # Restore the original full names for origin and destination
             [origin_place, destination_place].each do |place|
-              if place[:google_place_attributes] && place[:google_place_attributes][:original_name].present?
-                place[:name] = place[:google_place_attributes][:original_name]
+              if place[:original_name].present?
+                place[:name] = place[:original_name]
               end
             end
 
