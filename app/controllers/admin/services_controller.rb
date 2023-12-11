d817e59812831136b63e06ffce531fbb9333fd0c
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2023-12-11 17:36:17 -0600

    FMRPA-204

--- app/controllers/admin/services_controller.rb
+++ app/controllers/admin/services_controller.rb
@@ -21,7 +21,11 @@ class Admin::ServicesController < Admin::AdminController
   end
 
   def destroy
-    @service.archive # Makes service invisible in default scope
+    if @service.destroy
+      flash[:success] = "Service was successfully deleted."
+    else
+      flash[:error] = "Service could not be deleted."
+    end
     redirect_to admin_services_path
   end
 
@@ -132,21 +136,23 @@ class Admin::ServicesController < Admin::AdminController
   end
 
   def get_services_for_current_user
-    # Include only unarchived services by default
+    # NOTE: Includes unaffiliated Services by default
     if current_user.superuser?
-      @services.where(archived: false)
+      @services
     elsif current_user.currently_oversight?
       oa = current_user.staff_agency
-      Service.with_oversight_agency(oa).where(archived: false).order(agency_id: :desc)
+
+      Service.with_oversight_agency(oa).order(agency_id: :desc)
     elsif current_user.currently_transportation?
-      Service.where(agency_id: current_user.current_agency.id, archived: false)
+      Service.where(agency_id: current_user.current_agency.id)
+      # otherwise the current user is probably transportation staff
     elsif current_user.current_agency.nil? && current_user&.staff_agency&.oversight?
       # Return services with no transportation agency and oversight agency
-      Service.where(agency_id: nil, archived: false).select{|s| !s&.service_oversight_agency&.oversight_agency}
+      Service.where(agency_id: nil).select{|s| !s&.service_oversight_agency&.oversight_agency}
     else
-      Service.where(agency_id: current_user.staff_agency.id, archived: false)
+      Service.where(agency_id: current_user.staff_agency.id)
     end
-  end  
+  end
 
 
   def service_type
@@ -286,7 +292,7 @@ class Admin::ServicesController < Admin::AdminController
       @travel_pattern_services = []
     end
   end
-
+  
   def in_travel_patterns_mode?
     Config.dashboard_mode.to_sym == :travel_patterns
   end
