c0bf8596cf0cb3c32e5d3b7d17a372b9e758c1dd
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2023-12-21 10:58:23 -0600

    Allowing for locations with the same name before the first pipe to be allowed

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -52,29 +52,31 @@ module Api
         landmarks = landmarks.where(agency: agencies) if agencies.present?
 
         names = []
-        full_names = []
+        addresses = []
         landmarks.each do |landmark|
           full_name = landmark.name
           short_name = full_name.split('|').first.strip
-
+          address = landmark.formatted_address
+        
           # Skip if the search string matches any part of the name after the first pipe
           next if full_name.split('|', 2)[1]&.include?(search_string)
-
-          # Skip a POI if its full name is already in the list of full names, has no city, or has a bad city
-          next if full_names.include?(full_name) || landmark.city.in?(Trip::BAD_CITIES)
-
+        
+          # Skip a POI if its name and address combination is already in the list, has no city, or has a bad city
+          next if names.include?(short_name) && addresses.include?(address) || landmark.city.in?(Trip::BAD_CITIES)
+        
           # Create a modified google_place_hash with original_name
           modified_google_place_hash = landmark.google_place_hash
           modified_google_place_hash[:original_name] = full_name
-
+        
           # Append the modified hash to locations
           locations.append(modified_google_place_hash.merge(name: short_name))
-
+        
           names << short_name
-          full_names << full_name # Add full_name to the list of processed names
+          addresses << address # Add address to the list of processed addresses
           count += 1
           break if count >= max_results
         end
+        
         
         # User StompingGrounds
         # FMRPA-121 Just skip for now
