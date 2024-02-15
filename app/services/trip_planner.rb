1d2d99eb1c0e5ee0770ae3be3d4d13a16ec72827
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-02-15 15:32:38 -0600

    LIV-38

--- app/services/trip_planner.rb
+++ app/services/trip_planner.rb
@@ -87,9 +87,8 @@ class TripPlanner
     if @trip.user and @trip.user.age 
       @available_services = @available_services.by_max_age(@trip.user.age).by_min_age(@trip.user.age)
     end
-
     if @options[:purpose_id].present?
-      @available_services = @available_services.joins(:purposes).where(purposes: {id: @options[:purpose_id]})
+      @available_services = @available_services.joins(:purposes).where(purposes: { id: @options[:purpose_id] }).distinct
     end
 
     # Apply remaining filters if not in travel patterns mode.
