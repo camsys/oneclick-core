57bc8f91dfaf9154fe2f12a3eccc6708eaccb90b
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-03-06 10:00:29 -0600

    FMRPA-219

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -9,22 +9,19 @@ module Api
       # GET trips/past_trips
       # Returns past trips associated with logged in user, limit by max_results param
       def past_trips
-        past_trips_hash = @traveler.past_trips(params[:max_results] || 25)
-                                   .outbound
-                                   .map {|t| filter_trip_name(t)}
-        render status: 200, json: {trips: past_trips_hash}
+        past_start_date = Date.today - 30.days
+        past_end_date = Date.today
+        past_rides = EcolaneAmbassador.new.fetch_customer_orders(start_date: past_start_date, end_date: past_end_date)
+        render status: 200, json: {trips: past_rides}
       end
 
       # GET trips/future_trips
       # Returns future trips associated with logged in user, limit by max_results param
       def future_trips
-        # Only return trips that have been booked properly
-        future_trips_with_booking = @traveler.future_trips(params[:max_results] || 25).select do |trip|
-          trip.booking.present? && trip.booking.confirmation.present?
-        end
-
-        future_trips_hash = future_trips_with_booking.map { |t| filter_trip_name(t) }
-        render status: 200, json: {trips: future_trips_hash}
+        future_start_date = Date.today
+        future_end_date = Date.today + 14.days
+        future_rides = EcolaneAmbassador.new.fetch_customer_orders(start_date: future_start_date, end_date: future_end_date)
+        render status: 200, json: {trips: future_rides}
       end
 
       # POST trips/, POST itineraries/plan
