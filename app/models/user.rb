820c9f20ee0e2d5bbdd2793ad559eb59652244d8
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-03-06 16:42:17 -0600

    FMRPA-219

--- app/models/user.rb
+++ app/models/user.rb
@@ -146,7 +146,7 @@ class User < ApplicationRecord
 
   def fetch_ecolane_trips(start_date, end_date)
     # Initialize EcolaneAmbassador with user-specific details
-    ambassador = EcolaneAmbassador.new(service: self.current_service, user: self)
+    ambassador = EcolaneAmbassador.new(service: self.current_service, customer_id: self.booking_profile&.customer_id)
     # Make the API call
     ambassador.fetch_customer_orders(start: start_date.iso8601, end: end_date.iso8601)
   end
