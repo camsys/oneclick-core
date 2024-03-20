a6081d3ce9f37b656143e8168cac6cf3a3892d4d
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-03-20 09:31:52 -0500

    FMRPA-225 | Optimizing trips table query when location is included

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -113,10 +113,11 @@ class Trip < ApplicationRecord
   # Geographic scopes return trips that start or end in the passed geom
   # The geom parameter does not always serialize correctly to the geometry type, so converting to text.
   scope :origin_in, -> (geom) do
-    where(id: joins(:origin).where('ST_Within(waypoints.geom, ST_GeomFromText(?, 4326))', geom.as_text).pluck(:id))
+    joins(:origin).where('ST_Within(waypoints.geom, ?)', geom)
   end
+  
   scope :destination_in, -> (geom) do
-    where(id: joins(:destination).where('ST_Within(waypoints.geom, ST_GeomFromText(?, 4326))', geom.as_text).pluck(:id))
+    joins(:destination).where('ST_Within(waypoints.geom, ?)', geom)
   end
   
   # Returns trip that have any of the given purposes
