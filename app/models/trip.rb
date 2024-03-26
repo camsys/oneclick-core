759e0201acd2cc8224a2c3fec3f7918e809acef2
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-03-26 11:45:27 -0500

    FMRPA-226

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -112,15 +112,12 @@ class Trip < ApplicationRecord
 
   # Geographic scopes return trips that start or end in the passed geom
   # The geom parameter does not always serialize correctly to the geometry type, so converting to text.
-  scope :origin_in, -> (geom, distance) do
-    where(id: joins(:origin).where("ST_DWithin(waypoints.geom::geography, ST_GeomFromText(?, 4326)::geography, ?)", geom.as_text, distance).pluck(:id))
+  scope :origin_in, -> (geom) do
+    where(id: joins(:origin).where('ST_Within(waypoints.geom, ST_GeomFromText(?, 4326))', geom.as_text).pluck(:id))
   end
-
-  # Adjusted `destination_in` scope to use `ST_DWithin`
-  scope :destination_in, -> (geom, distance) do
-    where(id: joins(:destination).where("ST_DWithin(waypoints.geom::geography, ST_GeomFromText(?, 4326)::geography, ?)", geom.as_text, distance).pluck(:id))
+  scope :destination_in, -> (geom) do
+    where(id: joins(:destination).where('ST_Within(waypoints.geom, ST_GeomFromText(?, 4326))', geom.as_text).pluck(:id))
   end
-
   
   # Returns trip that have any of the given purposes
   scope :with_purpose, -> (purposes) do
