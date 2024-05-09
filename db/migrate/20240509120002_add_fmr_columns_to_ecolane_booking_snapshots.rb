02595b96f448e2f65e87734b2884fef705f11a46
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-05-09 17:53:43 -0500

    Adding in Ecolane Booking Snapshots

--- db/migrate/20240509120002_add_fmr_columns_to_ecolane_booking_snapshots.rb
+++ db/migrate/20240509120002_add_fmr_columns_to_ecolane_booking_snapshots.rb
@@ -1,11 +1,7 @@
 class AddFmrColumnsToEcolaneBookingSnapshots < ActiveRecord::Migration[5.0]
   def change
     change_table :ecolane_booking_snapshots do |t|
-      t.datetime :trip_time
       t.string :traveler
-      t.boolean :arrive_by
-      t.string :disposition_status
-      t.string :purpose
       t.string :orig_addr
       t.float :orig_lat
       t.float :orig_lng
@@ -16,11 +12,8 @@ class AddFmrColumnsToEcolaneBookingSnapshots < ActiveRecord::Migration[5.0]
       t.string :service_name
       t.integer :booking_client_id
       t.boolean :is_round_trip
-      t.datetime :booking_timestamp
-      t.string :funding_source
       t.string :sponsor
       t.integer :companions
-      t.string :trip_note
       t.string :ecolane_error_message
       t.boolean :pca
     end
