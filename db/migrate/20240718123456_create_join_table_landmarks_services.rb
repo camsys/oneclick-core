f317e339e687aaa54383bcd5cfb9868c835c5274
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-07-18 09:12:39 -0500

    WIP

--- db/migrate/20240718123456_create_join_table_landmarks_services.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableLandmarksAgencies < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :landmarks, :agencies do |t|
-      t.index :landmark_id
-      t.index :agency_id
-    end
-  end
-end
