830e9b729e14978f84b037e08b090d717b97ecdf
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-07-31 14:55:58 -0500

    Release 1.19.6 limiting count of processed landmarks

--- app/mailers/developer_mailer.rb
+++ app/mailers/developer_mailer.rb
@@ -2,7 +2,7 @@ class DeveloperMailer < ApplicationMailer
   default from: (ENV['SMTP_FROM_ADDRESS'] || "1-Click@camsys.com")
 
   def ecolane_summary_notification(summary, error_occurred = false)
-    @summary = summary
+    @summary = summary.map(&:html_safe)
     subject = error_occurred ? 'Ecolane POI Update Summary (Errors Occurred)' : 'Ecolane POI Update Summary'
     mail(to: 'jalen.w.sowell@gmail.com', subject: subject)
   end
