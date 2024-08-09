f72192abd28265afa828a11d13199a8539077cd8
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-08-09 09:40:40 -0500

    WIP

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -115,63 +115,64 @@ class Admin::ReportsController < Admin::AdminController
   
     # Fetch trips based on the current user's agency and role
     @trips = current_user.get_trips_for_staff_user
-    Rails.logger.info "Fetched trips for user. Number of trips: #{@trips.size}. Time elapsed: #{Time.now - start_time} seconds"
-    Rails.logger.info "First 200 trips: #{@trips.take(200).map(&:attributes)}"
+    fetch_time = Time.now - start_time
+    Rails.logger.info "Fetched trips for user. Number of trips: #{@trips.size}. Time elapsed: #{fetch_time} seconds"
+    Rails.logger.info "Trips: #{@trips.map(&:attributes)}"
   
     # Filter trips by date range
     @trips = @trips.from_date(@trip_time_from_date).to_date(@trip_time_to_date)
-    Rails.logger.info "Filtered trips by date. Number of trips: #{@trips.size}. Time elapsed: #{Time.now - start_time} seconds"
-    Rails.logger.info "First 200 trips after date filter: #{@trips.take(200).map(&:attributes)}"
+    filter_time = Time.now - start_time
+    Rails.logger.info "Filtered trips by date. Number of trips: #{@trips.size}. Time elapsed: #{filter_time} seconds"
+    Rails.logger.info "Trips: #{@trips.map(&:attributes)}"
   
     # Apply additional filters
     unless @purposes.empty?
       @trips = @trips.with_purpose(Purpose.where(id: @purposes).pluck(:name))
-      Rails.logger.info "Filtered trips by purpose. Number of trips: #{@trips.size}. Time elapsed: #{Time.now - start_time} seconds"
-      Rails.logger.info "First 200 trips after purpose filter: #{@trips.take(200).map(&:attributes)}"
+      purpose_filter_time = Time.now - start_time
+      Rails.logger.info "Filtered trips by purpose. Number of trips: #{@trips.size}. Time elapsed: #{purpose_filter_time} seconds"
+      Rails.logger.info "Trips: #{@trips.map(&:attributes)}"
     end
   
     unless @trip_origin_region.empty?
       @trips = @trips.origin_in(@trip_origin_region.geom)
-      Rails.logger.info "Filtered trips by origin region. Number of trips: #{@trips.size}. Time elapsed: #{Time.now - start_time} seconds"
-      Rails.logger.info "First 200 trips after origin region filter: #{@trips.take(200).map(&:attributes)}"
+      origin_filter_time = Time.now - start_time
+      Rails.logger.info "Filtered trips by origin region. Number of trips: #{@trips.size}. Time elapsed: #{origin_filter_time} seconds"
+      Rails.logger.info "Trips: #{@trips.map(&:attributes)}"
     end
   
     unless @trip_destination_region.empty?
       @trips = @trips.destination_in(@trip_destination_region.geom)
-      Rails.logger.info "Filtered trips by destination region. Number of trips: #{@trips.size}. Time elapsed: #{Time.now - start_time} seconds"
-      Rails.logger.info "First 200 trips after destination region filter: #{@trips.take(200).map(&:attributes)}"
+      destination_filter_time = Time.now - start_time
+      Rails.logger.info "Filtered trips by destination region. Number of trips: #{@trips.size}. Time elapsed: #{destination_filter_time} seconds"
+      Rails.logger.info "Trips: #{@trips.map(&:attributes)}"
     end
   
     unless @oversight_agency.blank?
       @trips = @trips.oversight_agency_in(@oversight_agency)
-      Rails.logger.info "Filtered trips by oversight agency. Number of trips: #{@trips.size}. Time elapsed: #{Time.now - start_time} seconds"
-      Rails.logger.info "First 200 trips after oversight agency filter: #{@trips.take(200).map(&:attributes)}"
+      oversight_filter_time = Time.now - start_time
+      Rails.logger.info "Filtered trips by oversight agency. Number of trips: #{@trips.size}. Time elapsed: #{oversight_filter_time} seconds"
+      Rails.logger.info "Trips: #{@trips.map(&:attributes)}"
     end
   
     if @trip_only_created_in_1click
       @trips = @trips.joins(itineraries: :booking)
                      .where(itineraries: { trip_type: 'paratransit' }, bookings: { created_in_1click: true })
-      Rails.logger.info "Filtered trips by 1-click creation. Number of trips: #{@trips.size}. Time elapsed: #{Time.now - start_time} seconds"
-      Rails.logger.info "First 200 trips after 1-click filter: #{@trips.take(200).map(&:attributes)}"
+      one_click_filter_time = Time.now - start_time
+      Rails.logger.info "Filtered trips by 1-click creation. Number of trips: #{@trips.size}. Time elapsed: #{one_click_filter_time} seconds"
+      Rails.logger.info "Trips: #{@trips.map(&:attributes)}"
     end
   
     # Order and limit the trips
     @trips = @trips.order(:trip_time).limit(CSVWriter::DEFAULT_RECORD_LIMIT)
-    Rails.logger.info "Ordered and limited trips. Number of trips: #{@trips.size}. Time elapsed: #{Time.now - start_time} seconds"
-    Rails.logger.info "First 2000 trips after ordering and limiting: #{@trips.take(2000).map(&:attributes)}"
+    order_time = Time.now - start_time
+    Rails.logger.info "Ordered and limited trips. Number of trips: #{@trips.size}. Time elapsed: #{order_time} seconds"
+    Rails.logger.info "Trips: #{@trips.map(&:attributes)}"
   
-    # Generate the CSV
-    respond_to do |format|
-      format.csv do
-        Rails.logger.info "Starting to generate CSV at #{Time.now}"
-        send_data @trips.to_csv(limit: CSVWriter::DEFAULT_RECORD_LIMIT, in_travel_patterns_mode: in_travel_patterns_mode?)
-        Rails.logger.info "Generated CSV in #{Time.now - start_time} seconds"
-      end
-    end
   
     total_time = Time.now - start_time
     Rails.logger.info "Completed trips_table method in #{total_time} seconds"
   end
+  
   
 
   def in_travel_patterns_mode?
