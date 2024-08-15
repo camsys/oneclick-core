53cb5705d5200bdb991e870e7f8472eae6d04ed9
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-08-15 10:50:29 -0500

    wip

--- app/mailers/developer_mailer.rb
+++ app/mailers/developer_mailer.rb
@@ -2,7 +2,7 @@ class DeveloperMailer < ApplicationMailer
   default from: (ENV['SMTP_FROM_ADDRESS'] || "1-Click@camsys.com")
 
   def ecolane_summary_notification(summary, error_occurred = false)
-    return unless ENV['JOB_NOTIFICATION_EMAIL'].present?
+    return unless ENV['JOB_SUMMARY_NOTIFICATION_EMAIL'].present?
     @summary = summary.map(&:html_safe)
     mail(to: ENV['JOB_SUMMARY_NOTIFICATION_EMAIL'], subject: 'Ecolane POI Update Summary')
   end
