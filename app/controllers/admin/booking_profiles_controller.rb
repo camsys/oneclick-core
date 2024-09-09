0482de46bd46ef27c16adefc7f048b54e755aae6
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-09-09 15:36:58 -0500

    FMRPA-233 | making sure dropdown respect service selection

--- app/controllers/admin/booking_profiles_controller.rb
+++ app/controllers/admin/booking_profiles_controller.rb
@@ -3,13 +3,9 @@ class Admin::BookingProfilesController < ApplicationController
   before_action :get_admin_pages
   before_action :ensure_travel_patterns_mode
 
-
   def index
     if current_user.superuser?
       @booking_profiles = UserBookingProfile.all
-    elsif current_user.oversight_admin? || current_user.oversight_staff? || current_user.transportation_admin? || current_user.transportation_staff? || current_user.staff? || current_user.partner_staff? || current_user.partner_admin?
-      ag_ids = @agency_map.map {|name, id| id} # Get agency ids from the agency map
-      @booking_profiles = UserBookingProfile.includes(service: :agency).where(services: {agency_id: ag_ids})
     else
       selected_agency_id = session[:selected_agency_id] || current_user.current_agency&.id
       ag_ids = [selected_agency_id].compact
