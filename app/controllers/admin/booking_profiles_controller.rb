e387ff7b7adfedc78cf91cf46a5f08a21943cfc6
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-09-12 16:23:23 -0500

    FMRPA-233 | Admin Console Booking Profiles page does not respect top agency dropdown selection

--- app/controllers/admin/booking_profiles_controller.rb
+++ app/controllers/admin/booking_profiles_controller.rb
@@ -4,12 +4,22 @@ class Admin::BookingProfilesController < ApplicationController
   before_action :ensure_travel_patterns_mode
 
   def index
-    if current_user.superuser?
+    selected_agency_id = session[:selected_agency_id] || current_user.current_agency&.id
+
+    if current_user.superuser? || selected_agency_id.blank?
       @booking_profiles = UserBookingProfile.all
     else
-      selected_agency_id = session[:selected_agency_id] || current_user.current_agency&.id
-      ag_ids = [selected_agency_id].compact
-      @booking_profiles = UserBookingProfile.includes(service: :agency).where(services: { agency_id: ag_ids })
+      # Find selected agency
+      selected_agency = Agency.find_by(id: selected_agency_id)
+
+      if selected_agency.is_a?(OversightAgency)
+        # If the selected agency is an oversight agency, get all booking profiles for its associated agencies
+        agency_ids = AgencyOversightAgency.where(oversight_agency_id: selected_agency.id).pluck(:transportation_agency_id)
+        @booking_profiles = UserBookingProfile.includes(service: :agency).where(services: { agency_id: agency_ids })
+      else
+        # Otherwise, get the booking profiles for the selected agency's services
+        @booking_profiles = UserBookingProfile.includes(service: :agency).where(services: { agency_id: selected_agency.id })
+      end
     end
   end
 
