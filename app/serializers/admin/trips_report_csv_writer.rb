2bd11cc32630e1f202b2a853148cc958f2934907
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-09-24 09:26:10 -0500

    FMRPA-264

--- app/serializers/admin/trips_report_csv_writer.rb
+++ app/serializers/admin/trips_report_csv_writer.rb
@@ -145,10 +145,8 @@ module Admin
       initial_status = booking_snapshot&.disposition_status
 
       # If the actual booking shows ecolane denial but the snapshot shows success, mark it as a round-trip denial.
-      if actual_status == Trip::DISPOSITION_STATUSES[:ecolane_denied] && initial_status == Trip::DISPOSITION_STATUSES[:ecolane_booked]
-        return Trip::DISPOSITION_STATUSES[:cancelled_round_trip_booking_denial]
-      elsif Config.dashboard_mode.to_sym == :travel_patterns && params[:ecolane_denied_trips_only].to_bool
-        return nil if initial_status == Trip::DISPOSITION_STATUSES[:ecolane_booked]
+      if Config.dashboard_mode.to_sym == :travel_patterns && params[:ecolane_denied_trips_only].to_bool
+        return nil unless actual_status == Trip::DISPOSITION_STATUSES[:ecolane_denied] && initial_status == Trip::DISPOSITION_STATUSES[:ecolane_denied]
       end
 
       actual_status || initial_status || 'Unknown Disposition'
