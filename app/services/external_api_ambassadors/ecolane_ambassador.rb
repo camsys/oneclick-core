ca3015e0959c5b62d44acf5b89b6bec4f4128fc0
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-09-26 13:37:06 -0500

    release/2.9.8

--- app/services/external_api_ambassadors/ecolane_ambassador.rb
+++ app/services/external_api_ambassadors/ecolane_ambassador.rb
@@ -402,18 +402,14 @@ class EcolaneAmbassador < BookingAmbassador
     end
     # err on new qa is response didn't finish building
     resp = send_request(url, 'POST', order)
-    return nil if resp.code != "200"
     if resp.body.nil? || resp.body.empty?
       Rails.logger.error "Received an empty or incomplete response body from Ecolane."
       return nil
     end
+    return nil if resp.code != "200"
     resp = Hash.from_xml(resp.body) || {}
     
     fare = resp.with_indifferent_access.fetch(:fare, {})
-    if fare.blank?
-      Rails.logger.error "Fare information missing from Ecolane response."
-      return nil
-    end
     (fare[:client_copay].to_f + fare[:additional_passenger].to_f) / 100
   end
 
@@ -500,12 +496,6 @@ class EcolaneAmbassador < BookingAmbassador
       Rails.logger.info '------Response from Ecolane---------'
       Rails.logger.info "Code: #{resp.code}"
       Rails.logger.info resp.body
-
-      if resp.body.nil? || resp.body.empty?
-        error_message = "Error: Received empty or incomplete response body from Ecolane."
-        Rails.logger.error error_message
-        raise error_message # Halt the process here by raising an error
-      end
   
       unless resp.is_a?(Net::HTTPSuccess)
         error_message = "Error from Ecolane: Code #{resp.code}, Message: #{resp.body}"
