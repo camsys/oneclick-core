a019c073151cf433cf8ef873a75a40fc4d33b12f
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-09-30 17:07:21 -0500

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -197,7 +197,6 @@ class User < ApplicationRecord
   ##
   # TODO(Drew) write documentation comment
   def get_services
-    county = county_name_if_ecolane_email
     if county.nil?
       # County name may be null if user has set email to a non-Ecolane email address.
       # Search for county that user logged in as from most recent user booking profile.
