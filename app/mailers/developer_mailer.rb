45b457593cee1c639b932aedc0a8f1b6c1d39bec
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-10-01 09:27:55 -0500

    WIP

--- app/mailers/developer_mailer.rb
+++ app/mailers/developer_mailer.rb
@@ -2,17 +2,11 @@ class DeveloperMailer < ApplicationMailer
   default from: (ENV['SMTP_FROM_ADDRESS'] || "1-Click@camsys.com")
 
   def ecolane_summary_notification(summary, error_occurred = false)
-    # Print the ENV variables and other important information
-    puts "JOB_SUMMARY_NOTIFICATION_EMAIL: #{ENV['JOB_SUMMARY_NOTIFICATION_EMAIL']}"
-    puts "SMTP_FROM_ADDRESS: #{ENV['SMTP_FROM_ADDRESS'] || '1-Click@camsys.com'}"
-    
-    # Check if the email address is present
-    if ENV['JOB_SUMMARY_NOTIFICATION_EMAIL'].present?
-      @summary = summary.map(&:html_safe)
-      mail(to: ENV['JOB_SUMMARY_NOTIFICATION_EMAIL'], subject: 'Ecolane POI Update Summary')
-      puts "Email sent to: #{ENV['JOB_SUMMARY_NOTIFICATION_EMAIL']}"
-    else
-      puts "JOB_SUMMARY_NOTIFICATION_EMAIL is not set, email not sent."
-    end
+    @summary = summary.map(&:html_safe)
+    environment_name = ENV['AWS_BUCKET'] || 'Unknown environment'
+    mail(
+      to: ENV['JOB_SUMMARY_NOTIFICATION_EMAIL'],
+      subject: "Ecolane POI Update Summary (#{environment_name})"
+    )
   end
 end
