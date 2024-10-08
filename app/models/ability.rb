330d204974a0add1d6ec4d9a438597d816cfb8f7
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-10-08 09:26:38 -0500

    WIP

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -139,7 +139,7 @@ class Ability
         can :manage, Purpose, agency_id: user.staff_agency.try(:id)
         can :manage, FundingSource, agency_id: user.staff_agency.try(:id)
         can :manage, BookingWindow, agency_id: user.staff_agency.try(:id)
-        can :manage, GeographyRecord
+        can :manage, GeographyRecord, agency_id: user.staff_agency.try(:id)
 
         # Can access services associated with an oversight agency, and those with no oversight agency
         can :manage, Service,
