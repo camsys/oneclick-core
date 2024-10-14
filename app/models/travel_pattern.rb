0e5b617195916d6a7bb7acfd13141683b12c6677
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-10-14 09:31:21 -0500

    WIP

--- app/models/travel_pattern.rb
+++ app/models/travel_pattern.rb
@@ -64,23 +64,29 @@ class TravelPattern < ApplicationRecord
       
         Rails.logger.info "Original Origin Zone ID: #{actual_origin_zone}, Original Destination Zone ID: #{actual_destination_zone}"
       
-        # Ensure destination zone matches as well in case of regular or reverse trip
-        if actual_origin_zone == actual_destination_zone && queried_origin.include?(actual_origin_zone) && queried_destination.include?(actual_destination_zone)
+        # Determine the source of the match
+        origin_match = queried_origin.include?(actual_origin_zone)
+        Rails.logger.info "Origin match: #{origin_match}"
+        destination_match = queried_destination.include?(actual_destination_zone)
+        Rails.logger.info "Destination match: #{destination_match}"
+      
+        # Check if both origin and destination are the same zone and allowed
+        if actual_origin_zone == actual_destination_zone && origin_match && destination_match
           Rails.logger.info "Allowing same-zone trip for pattern ID: #{pattern.id} where both origin and destination are #{actual_origin_zone}"
           true
       
         # Disallow trips where both origin and destination are in the queried zones but not the same
-        elsif queried_origin.include?(actual_origin_zone) && queried_destination.include?(actual_destination_zone) && actual_origin_zone != actual_destination_zone
+        elsif origin_match && destination_match && actual_origin_zone != actual_destination_zone
           Rails.logger.info "Skipping pattern ID: #{pattern.id} due to both origin and destination being in the queried zones but not the same"
           false
       
         # Allow reverse trips when allowed and zones are different
-        elsif queried_origin.include?(actual_destination_zone) && actual_origin_zone != actual_destination_zone && pattern.allow_reverse_sequence_trips
+        elsif destination_match && origin_match && actual_origin_zone != actual_destination_zone && pattern.allow_reverse_sequence_trips
           Rails.logger.info "Allowing reverse trip for pattern ID: #{pattern.id} from destination to origin"
           true
       
         # Allow regular trips from origin to destination only if destination is in the queried zones
-        elsif queried_origin.include?(actual_origin_zone) && queried_destination.include?(actual_destination_zone) && !queried_origin.include?(actual_destination_zone)
+        elsif origin_match && destination_match && !queried_origin.include?(actual_destination_zone)
           Rails.logger.info "Allowing regular trip for pattern ID: #{pattern.id} from origin to destination"
           true
       
