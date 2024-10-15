efdea4288f92c2fc80173726b719e9f461213bac
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-10-15 12:19:33 -0500

    WIP

--- app/services/trip_planner.rb
+++ app/services/trip_planner.rb
@@ -116,14 +116,14 @@ class TripPlanner
       non_paratransit_services = @available_services.where.not(type: 'Paratransit')
       @available_services = non_paratransit_services.or(paratransit_services)
     else
-      # Currently there's only one service per county, users are only allowed to book rides for their home service, and er only use paratransit services, so this may break
+      # Currently there's only one service per county, users are only allowed to book rides for their home service, and we only use paratransit services, so this may break
       options = {}
       options[:origin] = {lat: @trip.origin.lat, lng: @trip.origin.lng} if @trip.origin
       options[:destination] = {lat: @trip.destination.lat, lng: @trip.destination.lng} if @trip.destination
       options[:purpose_id] = @trip.purpose_id if @trip.purpose_id
       options[:date] = @trip.trip_time.to_date if @trip.trip_time
       
-      @available_services.joins(:travel_patterns).merge(TravelPattern.available_for(options)).distinct
+      @available_services = @available_services.joins(:travel_patterns).merge(TravelPattern.available_for(options)).distinct
       @relevant_eligibilities = (@available_services.collect { |service| service.eligibilities }).flatten.uniq.sort_by{ |elig| elig.rank }
       @relevant_accommodations = Accommodation.all.ordered_by_rank
       @available_services = @available_services.available_for(@trip, only_by: [:eligibility]) #, :accommodation])
